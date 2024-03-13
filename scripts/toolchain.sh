60411979ded9b9de98e240900c13905f62226367
Bastian Asmussen <bastian@asmussen.tech>
2024-03-13 08:14:30 +0100

    Update Rust config.

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -7,8 +7,9 @@ sudo pacman -S --noconfirm \
 
 # Install Rust toolchain.
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-rustup component add rust-analyzer
 
-# Install sccache.
-cargo install sccache
+# Source the environment.
+source $HOME/.cargo/env
+
+rustup component add rust-analyzer
 
