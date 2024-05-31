060f393fa85085e05ed97e76349f1a04eb76013e
Bastian Asmussen <bastian@asmussen.tech>
2024-05-31 12:49:39 +0200

    Update NixOS config.

--- flake.nix
+++ flake.nix
@@ -1,20 +1,114 @@
 {
-  description = "Nixos config flake";
+  description = "Top level NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Nixpkgs
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Unstable Packages
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Disko
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Home Manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
+  outputs = { self, nixpkgs, disko, home-manager, nixpkgs-unstable, ... }@inputs: let
+    inherit (self) outputs;
+
+    systems = [
+      "x86_64-linux"
+    ];
+
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
+
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in  {
+
+    overlays.additions = final: _prev: import ./pkgs final.pkgs;
+
+    overlays.unstable = final: prev: {
+      unstable = import nixpkgs-unstable {
+        system = prev.system;
+        config.allowUnfree = prev.config.allowUnfree;
+      };
+    };
+
+    nixpkgs.overlays = [
+      self.overlays.unstable
+      alacritty-theme.overlays.default
+      templ.overlays.default
+    ];
+
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+
+    nixosConfigurations.overlord = nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        meta = { hostname = "overlord"; };
+      };
+      system = "x86_64-linux";
       modules = [
-        ./hosts/default/configuration.nix
+        disko.nixosModules.disko
+
+        ./machines/overlord/hardware-configuration.nix
+        ./machines/overlord/disko-config.nix
+        ./configuration.nix
+
+        ({ config, pkgs, ...}: {
+          nixpkgs.overlays = [
+            self.overlays.unstable
+          ];
+        })
+        home-manager.nixosModules.home-manager
+        {
+          # home-manager.useGlobalPkgs = true;
+          # home-manager.useUserPackages = true;
+          home-manager.users.bastian = import ./home/home.nix;
+        }
+      ];
+    };
+    nixosConfigurations.work = nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit inputs outputs;
+        meta = { hostname = "work"; };
+      };
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+
+        ./machines/work/hardware-configuration.nix
+        ./machines/work/disko-config.nix
+        ./configuration.nix
+
+        ({ config, pkgs, ...}: {
+          nixpkgs.overlays = [
+            self.overlays.unstable
+            self.overlays.additions
+          ];
+        })
+        home-manager.nixosModules.home-manager
+        {
+          # home-manager.useGlobalPkgs = true;
+          # home-manager.useUserPackages = true;
+          home-manager.users.bastian = import ./home/home.nix;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+        }
+      ];
+    };
+
+    homeConfigurations.bastian = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = { inherit inputs; };
+
+      modules = [
+	      ./home.nix
       ];
     };
   };
---
+++ home/zsh.nix
@@ -0,0 +1,71 @@
+{ config, pkgs, ... }:
+{
+  enable = true;
+
+  history = {
+    size = 10000;
+    path = "${config.xdg.dataHome}/zsh/history";
+  };
+
+  shellAliases = {
+    vim = "nvim";
+    ls = "ls --color";
+    c = "clear";
+  };
+
+  initExtra = ''
+    bindkey -e
+
+    [[ ! -f ${./p10k.zsh} ]] || source ${./p10k.zsh}
+
+    # disable sort when completing `git checkout`
+    zstyle ':completion:*:git-checkout:*' sort false
+    # set descriptions format to enable group support
+    # NOTE: don't use escape sequences here, fzf-tab will ignore them
+    zstyle ':completion:*:descriptions' format '[%d]'
+    # set list-colors to enable filename colorizing
+    zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+    # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+    zstyle ':completion:*' menu no
+    # preview directory's content with eza when completing cd
+    zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -1 --color=always $realpath'
+    zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+    zstyle ':fzf-tab:complete:ls:*' fzf-preview 'cat $realpath'
+    # switch group using `<` and `>`
+    zstyle ':fzf-tab:*' switch-group '<' '>'
+  '';
+  oh-my-zsh = {
+    enable = false;
+    plugins = [
+       "git" "sudo" "golang" "kubectl" "kubectx" "rust" "command-not-found"
+    ];
+  };
+  plugins = [
+    {
+      name = "zsh-autosuggestions";
+      src = pkgs.zsh-autosuggestions;
+      file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+    }
+    {
+      name = "zsh-completions";
+      src = pkgs.zsh-completions;
+      file = "share/zsh-completions/zsh-completions.zsh";
+    }
+    {
+      name = "zsh-syntax-highlighting";
+      src = pkgs.zsh-syntax-highlighting;
+      file = "share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh";
+    }
+    {
+      name = "powerlevel10k";
+      src = pkgs.zsh-powerlevel10k;
+      file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    }
+    {
+      name = "fzf-tab";
+      src = pkgs.zsh-fzf-tab;
+      file = "share/fzf-tab/fzf-tab.plugin.zsh";
+    }
+  ];
+}
+}
