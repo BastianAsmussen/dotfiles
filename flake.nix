31dcadd95bb7855d9492877f9d76ff94e8bbfcc1
Bastian Asmussen <bastian@asmussen.tech>
2024-06-10 00:44:15 +0200

    Create flake.

--- flake.nix
+++ flake.nix
@@ -1,23 +1,23 @@
 {
-  description = "Top level NixOS Flake";
+  description = "Top level NixOS Flake.";
 
   inputs = {
-    # Nixpkgs
+    # Nixpkgs.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # Unstable Packages
+    # Unstable Packages.
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # Disko
+    # Disko.
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
-    # Home Manager
+    # Home Manager.
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, disko, home-manager, nixpkgs-unstable, ... }@inputs: let
+  outputs = { self, nixpkgs, nixpkgs-unstable, disko, home-manager, ... }@inputs: let
     inherit (self) outputs;
 
     systems = [
@@ -26,14 +26,12 @@
 
     pkgs = import nixpkgs {
       system = "x86_64-linux";
+      
       config.allowUnfree = true;
     };
 
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in  {
-
-    overlays.additions = final: _prev: import ./pkgs final.pkgs;
-
     overlays.unstable = final: prev: {
       unstable = import nixpkgs-unstable {
         system = prev.system;
@@ -43,62 +41,32 @@
 
     nixpkgs.overlays = [
       self.overlays.unstable
-      alacritty-theme.overlays.default
-      templ.overlays.default
     ];
 
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-
-    nixosConfigurations.overlord = nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        meta = { hostname = "overlord"; };
-      };
-      system = "x86_64-linux";
-      modules = [
-        disko.nixosModules.disko
-
-        ./machines/overlord/hardware-configuration.nix
-        ./machines/overlord/disko-config.nix
-        ./configuration.nix
-
-        ({ config, pkgs, ...}: {
-          nixpkgs.overlays = [
-            self.overlays.unstable
-          ];
-        })
-        home-manager.nixosModules.home-manager
-        {
-          # home-manager.useGlobalPkgs = true;
-          # home-manager.useUserPackages = true;
-          home-manager.users.bastian = import ./home/home.nix;
-        }
-      ];
-    };
-    nixosConfigurations.work = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.limitless = nixpkgs.lib.nixosSystem {
       specialArgs = {
         inherit inputs outputs;
-        meta = { hostname = "work"; };
+
+        meta = { hostname = "limitless"; };
       };
       system = "x86_64-linux";
       modules = [
+        # Modules.
         disko.nixosModules.disko
-
-        ./machines/work/hardware-configuration.nix
-        ./machines/work/disko-config.nix
+      	
+	# System Specific.
+      	./machines/limitless/hardware-configuration.nix
+        ./machines/limitless/disko-config.nix
+        
+	# General.
         ./configuration.nix
 
-        ({ config, pkgs, ...}: {
-          nixpkgs.overlays = [
-            self.overlays.unstable
-            self.overlays.additions
-          ];
-        })
+	# Home Manager.
         home-manager.nixosModules.home-manager
         {
-          # home-manager.useGlobalPkgs = true;
-          # home-manager.useUserPackages = true;
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
           home-manager.users.bastian = import ./home/home.nix;
-          home-manager.extraSpecialArgs = { inherit inputs; };
         }
       ];
     };
@@ -108,8 +76,9 @@
       extraSpecialArgs = { inherit inputs; };
 
       modules = [
-	      ./home.nix
+	./home.nix
       ];
     };
   };
 }
+
