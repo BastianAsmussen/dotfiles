bcdc6151f3e072b557c33f5575456f09bfd39c74
Bastian Asmussen <bastian@asmussen.tech>
2024-06-18 07:43:11 +0200

    Move to catppuccin theme.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,18 +3,14 @@ return {
 		"folke/tokyonight.nvim",
 		name = "tokyonight",
 		priority = 1000,
-		config = function()
-			vim.cmd([[colorscheme tokyonight-night]])
-		end,
 	},
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme catppuccin-mocha]])
+		end,
+
 	},
 }
