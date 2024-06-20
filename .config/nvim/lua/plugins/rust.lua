ff179a746366535082423428091f9697c7431cb2
Bastian Asmussen <bastian@asmussen.tech>
2024-06-20 09:16:14 +0200

    Add format on save.

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -2,12 +2,19 @@ return {
   {
     {
       'mrcjkb/rustaceanvim',
-      version = '^4', -- Recommended
-      lazy = false, -- This plugin is already lazy
+      version = '^4', -- Recommended.
+      lazy = false, -- This plugin is already lazy.
+    },
+    {
+      'rust-lang/rust.vim',
+      ft = 'rust',
+      init = function()
+        vim.g.rustfmt_autosave = 1
+      end
     },
     {
       'saecki/crates.nvim',
-      ft = { "rust", "toml" },
+      ft = { 'rust', 'toml' },
       config = function(_, opts)
         local crates = require('crates')
         crates.setup(opts)
