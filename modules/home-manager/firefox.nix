0fe9aae796a74be44243753996b92c9cc1aba512
Bastian Asmussen <bastian@asmussen.tech>
2024-06-21 01:41:19 +0200

    Update Firefox settings.

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -6,7 +6,14 @@
 }: {
   programs.firefox = {
     enable = true;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+    };
+    
     profiles.bastian = {
+      search.default = "DuckDuckGo";
+      search.force = true;
       search.engines = {
         "Nix Packages" = {
           urls = [{
@@ -20,13 +27,29 @@
           icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
           definedAliases = ["@np"];
         };
+        
+        "NixOS Wiki" = {
+           urls = [{ template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; }];
+           
+           iconUpdateURL = "https://wiki.nixos.org/favicon.ico";
+           updateInterval = 24 * 60 * 60 * 1000; # every day
+           definedAliases = [ "@nw" ];
+         };
+         
+        "Bing".metaData.hidden = true;
+        "Google".metaData.hidden = true;
       };
-      search.force = true;
+      
+      settings = {
+        "privacy.globalprivacycontrol.enabled" = true;
+        "privacy.donottrackheader.enabled" = true;
+      };
       
       extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
         bitwarden
         ublock-origin
         clearurls
+        duckduckgo-privacy-essentials
         sponsorblock
         return-youtube-dislikes
         darkreader
