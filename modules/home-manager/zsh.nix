5087923596ee8b6b045d80b57211107a2d0b168c
Bastian Asmussen <bastian@asmussen.tech>
2024-06-21 06:06:59 +0200

    Begin migrating ZSH config.

---
+++ modules/home-manager/zsh.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    
+    history = {
+      size = 10000;
+      history.path = "${config.xdg.dataHome}/zsh/history";
+    };
+    
+    shellAliases = {
+      vi = "vim";
+      vim = "nvim";
+      
+      ls = "ls --color";
+      c = "clear";
+      
+      cp = "cp -r";
+      rm = "rm -r";
+    };
+  };
+}
+
