1deed2d48fa4085cce9a10e9fa549e077943ebcc
Bastian Asmussen <bastian@asmussen.tech>
2024-06-28 17:34:41 +0200

    add zsh plugins

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -13,11 +13,51 @@
     };
 
     shellAliases = {
+      vim = "nvim";
+      vi = "vim";
+
       ls = "ls --color";
       c = "clear";
 
       cp = "cp -r";
       rm = "rm -r";
     };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "sudo"
+        "golang"
+        "kubectl"
+        "kubectx"
+        "rust"
+        "command-not-found"
+        "pass"
+      ];
+    };
+
+    plugins = [
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.zsh-syntax-highlighting;
+        file = "share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh";
+      }
+      {
+        name = "zsh-completions";
+        src = pkgs.zsh-completions;
+        file = "share/zsh-completions/zsh-completions.zsh";
+      }
+      {
+        name = "zsh-autosuggestions";
+        src = pkgs.zsh-autosuggestions;
+        file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+      }
+      {
+        name = "fzf-tab";
+        src = pkgs.zsh-fzf-tab;
+        file = "share/fzf-tab/fzf-tab.plugin.zsh";
+      }
+    ];
   };
 }
