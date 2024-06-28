8eafc7f141a5828559bcfb667f22728026435669
Bastian Asmussen <bastian@asmussen.tech>
2024-06-28 16:30:51 +0200

    Ran Alejandra formatter.

--- flake.nix
+++ flake.nix
@@ -1,36 +1,46 @@
 {
   description = "Top-level flake.";
-  
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    
+
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
-    
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    
+
     stylix.url = "github:danth/stylix";
-    
+
     firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
     firefox-addons.inputs.nixpkgs.follows = "nixpkgs";
   };
-  
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      nixosConfigurations.limitless = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/limitless/configuration.nix
-          
-          inputs.disko.nixosModules.disko
-          inputs.home-manager.nixosModules.default
-          inputs.stylix.nixosModules.stylix
-        ];
-      };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    nixosConfigurations.limitless = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/limitless/configuration.nix
+
+        inputs.disko.nixosModules.disko
+        inputs.home-manager.nixosModules.default
+        inputs.stylix.nixosModules.stylix
+      ];
     };
+  };
 }
--- hosts/limitless/configuration.nix
+++ hosts/limitless/configuration.nix
@@ -1,15 +1,19 @@
-{ config, lib, pkgs, inputs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: {
   imports = [
     ./hardware-configuration.nix
     ./disko-config.nix
-    
+
     inputs.home-manager.nixosModules.default
   ];
-  
+
   nix.settings.experimental-features = ["nix-command" "flakes"];
-  
+
   boot.loader = {
     efi = {
       canTouchEfiVariables = true;
@@ -21,7 +25,7 @@
       device = "nodev";
     };
   };
-  
+
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
@@ -33,13 +37,12 @@
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
-
     # Modesetting is required.
     modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
     powerManagement.enable = false;
 
@@ -49,28 +52,28 @@
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
 
     # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
+    # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.production;
   };
-  
+
   nixpkgs.config.allowUnfree = true;
-  
+
   networking.hostName = "limitless";
   networking.networkmanager.enable = true;
-  
+
   time.timeZone = "Europe/Copenhagen";
-  
+
   i18n.defaultLocale = "en_DK.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_DK.UTF-8";
@@ -83,14 +86,14 @@
     LC_TELEPHONE = "en_DK.UTF-8";
     LC_TIME = "en_DK.UTF-8";
   };
-  
+
   services.xserver = {
     enable = true;
 
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
-  
+
   # Enable sound.
   # hardware.pulseaudio.enable = true;
   # OR
@@ -98,15 +101,15 @@
   #   enable = true;
   #   pulse.enable = true;
   # };
-  
+
   users.users.bastian = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" "libvirt" ];
+    extraGroups = ["wheel" "docker" "libvirt"];
   };
-  
+
   home-manager = {
     backupFileExtension = "backup";
-    extraSpecialArgs = { inherit inputs; };
+    extraSpecialArgs = {inherit inputs;};
     users = {
       "bastian" = import ./home.nix;
     };
@@ -119,42 +122,43 @@
     mullvad-vpn
     qbittorrent
     mpv
+    discord
+    spotify
   ];
-  
+
   virtualisation = {
     docker.enable = true;
     libvirtd.enable = true;
   };
 
   programs.virt-manager.enable = true;
-  
+
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
   };
-  
+
   services.mullvad-vpn.enable = true;
   services.resolved.enable = true;
-  
+
   fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    (nerdfonts.override {fonts = ["JetBrainsMono"];})
     openmoji-color
   ];
-  
+
   stylix = {
     enable = true;
-    
+
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     image = ../../modules/wallpapers/wallpaper.png;
-    
+
     polarity = "dark";
-    
+
     cursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Ice";
     };
   };
-  
+
   system.stateVersion = "24.05";
 }
-
