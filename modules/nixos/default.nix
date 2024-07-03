7a53e5ff937d3dd3b3a490e73a2a7affa964a2fe
Bastian Asmussen <bastian@asmussen.tech>
2024-07-03 16:09:30 +0200

    chore: move stylix to module

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -6,8 +6,11 @@
   imports = [
     ./nvidia.nix
     ./security.nix
+    ./stylix.nix
   ];
 
   gpg.enable = lib.mkDefault true;
   vpn.enable = lib.mkDefault true;
+
+  stylix.enable = lib.mkDefault true;
 }
