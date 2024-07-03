141278c237b7c9391c1e461f85ab79b0366c3525
Bastian Asmussen <bastian@asmussen.tech>
2024-07-03 15:55:30 +0200

    chore: move home-manager configuration

---
+++ modules/home-manager/home.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    ./alacritty.nix
+    ./firefox.nix
+    ./fzf.nix
+    ./git.nix
+    ./oh-my-posh.nix
+    ./tmux.nix
+    ./zoxide.nix
+    ./zsh.nix
+  ];
+
+  home = {
+    username = "bastian";
+    homeDirectory = "/home/bastian";
+
+    stateVersion = "24.05";
+  };
+
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs.
+  systemd.user.startServices = "sd-switch";
+}
