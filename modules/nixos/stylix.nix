b97a598352c26fb597514f137fc8d33ec16f0627
Bastian Asmussen <bastian@asmussen.tech>
2024-07-05 15:29:28 +0200

    chore: enable GRUB image usage in stylix configuration

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -9,6 +9,8 @@
       base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
       image = ./../wallpapers/wallpaper.png;
 
+      targets.grub.useImage = true;
+      
       polarity = "dark";
 
       cursor = {
