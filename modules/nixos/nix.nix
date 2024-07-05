68e0c68797636f618ac508f24f424a1e672f0d9b
Bastian Asmussen <bastian@asmussen.tech>
2024-07-05 16:58:33 +0200

    chore: enable automatic system upgrades with daily schedule and build logs

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,8 +1,20 @@
-{
+{inputs, ...}: {
   nixpkgs.config.allowUnfree = true;
 
   nix.settings = {
     warn-dirty = false;
     experimental-features = ["nix-command" "flakes"];
+  };
+
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # Print build logs.
+    ];
+    dates = "daily";
+    randomizedDelaySec = "45min";
   };
 }
