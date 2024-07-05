91eba2dec2ba55eb40b30f355fde092561ff3430
Bastian Asmussen <bastian@asmussen.tech>
2024-07-05 15:37:57 +0200

    chore: refactor user configuration to separate module
    Moved user configuration for 'bastian' from the main configuration file to a dedicated module for better organization and maintainability.

--- hosts/limitless/configuration.nix
+++ hosts/limitless/configuration.nix
@@ -55,17 +55,6 @@
   #   pulse.enable = true;
   # };
 
-  users.users.bastian = {
-    isNormalUser = true;
-    extraGroups = ["wheel" "docker" "libvirt"];
-
-    shell = pkgs.zsh;
-    openssh.authorizedKeys.keys = [
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPc8Md/RuoiNaFIieZ2hTQ6z2R+bE8xealvVhs4omoq3AAAABHNzaDo= bastian@asmussen"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUlnObPZOCziYmtmSH/+lPBwQwEx8mpFh0YLF2YmBsO"
-    ];
-  };
-
   environment.systemPackages = with pkgs; [
     git
     gitui
---
+++ modules/nixos/user.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}:
+{
+  users.users.bastian = {
+    isNormalUser = true;
+    extraGroups = ["wheel" "docker" "libvirt"];
+    shell = pkgs.zsh;
+    description = "Bastian Asmussen";
+
+    openssh.authorizedKeys.keys = [
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPc8Md/RuoiNaFIieZ2hTQ6z2R+bE8xealvVhs4omoq3AAAABHNzaDo= bastian@asmussen"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUlnObPZOCziYmtmSH/+lPBwQwEx8mpFh0YLF2YmBsO"
+    ];
+  };
+}
+
