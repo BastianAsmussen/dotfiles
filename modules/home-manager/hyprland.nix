12c0e9617586853587410ce79003141fa7925244
Bastian Asmussen <bastian@asmussen.tech>
2024-07-09 00:33:46 +0200

    chore: enhance hyprland configuration with monitor setup and color customization

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   wayland.windowManager.hyprland = {
     enable = true;
 
@@ -12,43 +17,67 @@
 
       input.kb_layout = "dk";
 
-      bind = [
-        "$mod, RETURN, exec, $terminal"
-        "$mod, F, exec, $browser"
-
-        "$mod, Q, killactive"
-        "$mod, M, exit"
-
-        "$mod, H, movefocus, l"
-        "$mod, L, movefocus, r"
-        "$mod, K, movefocus, u"
-        "$mod, J, movefocus, d"
+      monitor = [
+        "DP-1, 1920x1080@240, 0x0, 1"
+        "Unknown-1, 1920x1080, -1920x0, 1"
+        ", preferred, auto, 1" # Recommended rule for quickly plugging in random monitors.
       ];
+
+      bind =
+        [
+          "$mod, RETURN, exec, $terminal"
+          "$mod, F, exec, $browser"
+
+          "$mod, Q, killactive"
+          "$mod, M, exit"
+
+          "$mod, H, movefocus, l"
+          "$mod, L, movefocus, r"
+          "$mod, K, movefocus, u"
+          "$mod, J, movefocus, d"
+        ]
+        ++ (
+          builtins.concatLists (builtins.genList (
+              x: let
+                ws = let
+                  c = (x + 1) / 10;
+                in
+                  builtins.toString (x + 1 - (c * 10));
+              in [
+                "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              ]
+            )
+            10)
+        );
 
       bindm = [
         "$mod, mouse:272, movewindow"
         "$mod, mouse:273, resizewindow"
         "$mod ALT, mouse:272, resizewindow"
       ];
+
+      general = with config.lib.stylix.colors; {
+        "col.active_border" = lib.mkForce "rgba(${base0E}ff) rgba(${base09}ff) 60deg";
+        "col.inactive_border" = lib.mkForce "rgba(${base00}ff)";
+
+        gaps_out = 30;
+      };
     };
 
     extraConfig = ''
-      bezier=easeOutBack,0.34,1.56,0.64,1
-      bezier=easeInBack,0.36,0,0.66,-0.56
-      bezier=easeInCubic,0.32,0,0.67,0
-      bezier=easeInOutCubic,0.65,0,0.35,1
+      bezier = easeOutBack, 0.34, 1.56, 0.64, 1
+      bezier = easeInBack, 0.36, 0, 0.66, -0.56
+      bezier = easeInCubic, 0.32, 0, 0.67 ,0
+      bezier = easeInOutCubic, 0.65, 0, 0.35, 1
 
-      animation=windowsIn,1,5,easeOutBack,popin
-      animation=windowsOut,1,5,easeInBack,popin
-      animation=fadeIn,0
-      animation=fadeOut,1,10,easeInCubic
-      animation=workspaces,1,4,easeInOutCubic,slide
+      animation = windowsIn, 1, 5, easeOutBack, popin
+      animation = windowsOut, 1, 5, easeInBack, popin
+      animation = fadeIn, 0
+      animation = fadeOut, 1, 10, easeInCubic
+      animation = workspaces, 1, 4, easeInOutCubic, slide
 
-      general:gaps_out=30
-
-      xwayland {
-        force_zero_scaling = true
-      }
+      xwayland:force_zero_scaling = true
     '';
   };
 }
