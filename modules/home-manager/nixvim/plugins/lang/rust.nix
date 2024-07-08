df988f7d8163ee6bf74ac26741e903e165e11b9a
Bastian Asmussen <bastian@asmussen.tech>
2024-07-08 16:33:15 +0200

    chore: update rust and lsp plugin configurations
    Removed rustaceanvim plugin and added configuration for rust-analyzer in LSP settings.

--- modules/home-manager/nixvim/plugins/lang/rust.nix
+++ modules/home-manager/nixvim/plugins/lang/rust.nix
@@ -2,9 +2,6 @@
   programs.nixvim = {
     globals.rustfmt_autosave = 1; # Format on save.
 
-    plugins = {
-      rustaceanvim.enable = true;
-      crates-nvim.enable = true;
-    };
+    plugins.crates-nvim.enable = true;
   };
 }
