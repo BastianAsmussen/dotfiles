aa56dd584df80d6e208a15a9107de327e4534301
Bastian Asmussen <bastian@asmussen.tech>
2024-07-08 21:48:44 +0200

    chore: add key bindings for terminal and window management in hyprland configuration

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -4,6 +4,22 @@
 
     settings = {
       "$mod" = "SUPER";
+      "$terminal" = "alacritty";
+
+      bind = [
+        "$mod, Q, exec, $terminal"
+
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+      ];
+
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+        "$mod ALT, mouse:272, resizewindow"
+      ];
     };
   };
 }
