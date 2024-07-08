d30ba13540e218bfebe1e23fa371f75c9af57ce1
Bastian Asmussen <bastian@asmussen.tech>
2024-07-09 00:33:29 +0200

    chore: add font configurations to NixOS module for better typography customization

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -17,6 +17,23 @@
         package = pkgs.bibata-cursors;
         name = "Bibata-Modern-Ice";
       };
+
+      fonts = {
+        monospace = {
+          package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+          name = "JetBrainsMono Nerd Font Mono";
+        };
+
+        sansSerif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Sans";
+        };
+
+        serif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Serif";
+        };
+      };
     };
   };
 }
