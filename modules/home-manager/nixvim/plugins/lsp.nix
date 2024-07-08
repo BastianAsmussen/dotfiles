c7525263472ce09232082f6b7d36749b2a36d4c6
Bastian Asmussen <bastian@asmussen.tech>
2024-07-08 14:05:34 +0200

    chore: refactor LSP configuration into a separate file for better modularity and maintainability

---
+++ modules/home-manager/nixvim/plugins/lsp.nix
@@ -0,0 +1,15 @@
+{
+  programs.nixvim.plugins.lsp = {
+    enable = true;
+
+    servers = {
+      rust-analyzer = {
+        enable = true;
+
+        # Provided by dev shells.
+        installRustc = false;
+        installCargo = false;
+      };
+    };
+  };
+}
