fdf60e06671f9a839df143d3144e04b91feea4fd
Bastian Asmussen <bastian@asmussen.tech>
2024-07-08 15:28:57 +0200

    chore: enable friendly-snippets plugin for better code completions

--- modules/home-manager/nixvim/plugins/completions.nix
+++ modules/home-manager/nixvim/plugins/completions.nix
@@ -1,12 +1,11 @@
 {
   programs.nixvim.plugins = {
     luasnip.enable = true;
-
+    cmp.enable = true;
     lspkind = {
       enable = true;
       cmp.enable = true;
     };
-
-    cmp.enable = true;
+    friendly-snippets.enable = true;
   };
 }
