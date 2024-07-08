c35c8e12869366ac7e9990c417f1a8952fac125c
Bastian Asmussen <bastian@asmussen.tech>
2024-07-08 15:31:02 +0200

    chore: simplify LSP server configuration by removing rust-analyzer settings

--- modules/home-manager/nixvim/plugins/lsp.nix
+++ modules/home-manager/nixvim/plugins/lsp.nix
@@ -2,16 +2,6 @@
   programs.nixvim.plugins.lsp = {
     enable = true;
 
-    servers = {
-      rust-analyzer = {
-        enable = true;
-
-        # Provided by dev shells.
-        installRustc = false;
-        installCargo = false;
-      };
-
-      nixd.enable = true;
-    };
+    servers.nixd.enable = true;
   };
 }
