ada1517449287f6cb606f19e0e3b54b12d7716df
Bastian Asmussen <bastian@asmussen.tech>
2024-07-08 22:46:09 +0200

    chore: enhance Hyprland configuration with additional settings and keybindings
    - Enabled xwayland and set specific paths for terminal and browser.
    - Added keyboard layout and new keybindings for window management.
    - Included extra configuration for animations and general settings.
    - Enabled polkit for security.

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -1,18 +1,28 @@
-{
+{pkgs, ...}: {
   wayland.windowManager.hyprland = {
     enable = true;
 
+    xwayland.enable = true;
+
     settings = {
       "$mod" = "SUPER";
-      "$terminal" = "alacritty";
+
+      "$terminal" = "${pkgs.alacritty}/bin/alacritty";
+      "$browser" = "${pkgs.firefox}/bin/firefox";
+
+      input.kb_layout = "dk";
 
       bind = [
-        "$mod, Q, exec, $terminal"
+        "$mod, RETURN, exec, $terminal"
+        "$mod, F, exec, $browser"
 
-        "$mod, left, movefocus, l"
-        "$mod, right, movefocus, r"
-        "$mod, up, movefocus, u"
-        "$mod, down, movefocus, d"
+        "$mod, Q, killactive"
+        "$mod, M, exit"
+
+        "$mod, H, movefocus, l"
+        "$mod, L, movefocus, r"
+        "$mod, K, movefocus, u"
+        "$mod, J, movefocus, d"
       ];
 
       bindm = [
@@ -21,5 +31,24 @@
         "$mod ALT, mouse:272, resizewindow"
       ];
     };
+
+    extraConfig = ''
+      bezier=easeOutBack,0.34,1.56,0.64,1
+      bezier=easeInBack,0.36,0,0.66,-0.56
+      bezier=easeInCubic,0.32,0,0.67,0
+      bezier=easeInOutCubic,0.65,0,0.35,1
+
+      animation=windowsIn,1,5,easeOutBack,popin
+      animation=windowsOut,1,5,easeInBack,popin
+      animation=fadeIn,0
+      animation=fadeOut,1,10,easeInCubic
+      animation=workspaces,1,4,easeInOutCubic,slide
+
+      general:gaps_out=30
+
+      xwayland {
+        force_zero_scaling = true
+      }
+    '';
   };
 }
