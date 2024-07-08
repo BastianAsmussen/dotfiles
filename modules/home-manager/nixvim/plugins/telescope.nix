b200843785ed8203cd6974f3913b23aa11501160
Bastian Asmussen <bastian@asmussen.tech>
2024-07-08 18:45:39 +0200

    feat: add telescope plugin configuration for enhanced file searching and navigation

---
+++ modules/home-manager/nixvim/plugins/telescope.nix
@@ -0,0 +1,33 @@
+{
+  programs.nixvim.plugins.telescope = {
+    enable = true;
+
+    keymaps = {
+      # Find files using Telescope command-line sugar.
+      "<leader>ff" = "find_files";
+      "<leader>fg" = "live_grep";
+      "<leader>b" = "buffers";
+      "<leader>fh" = "help_tags";
+      "<leader>fd" = "diagnostics";
+
+      # fzf like bindings.
+      "<C-p>" = "git_files";
+      "<leader>p" = "oldfiles";
+      "<C-f>" = "live_grep";
+    };
+
+    settings.defaults = {
+      file_ignore_patterns = [
+        "^.git/"
+        "^.mypy_cache/"
+        "^__pycache__/"
+        "^output/"
+        "^data/"
+        "%.ipynb"
+        "^target/"
+      ];
+
+      set_env.COLORTERM = "truecolor";
+    };
+  };
+}
