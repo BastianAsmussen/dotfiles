2453d249d77b0e018e5cf906bd211c318272a6d3
Bastian Asmussen <bastian@asmussen.tech>
2024-07-08 18:55:20 +0200

    chore: update lualine configuration to include additional sections for better status line information

--- modules/home-manager/nixvim/plugins/lualine.nix
+++ modules/home-manager/nixvim/plugins/lualine.nix
@@ -12,5 +12,18 @@
       left = "";
       right = "";
     };
+
+    sections = {
+      lualine_a = ["mode"];
+      lualine_b = ["branch"];
+      lualine_c = ["filename" "diff"];
+
+      lualine_x = [
+        "diagnostics"
+        "encoding"
+        "fileformat"
+        "filetype"
+      ];
+    };
   };
 }
