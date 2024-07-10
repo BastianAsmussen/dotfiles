2bc0ee07cc5b8925307115971e05dd23fe52ac87
Bastian Asmussen <bastian@asmussen.tech>
2024-07-10 18:51:21 +0200

    chore: integrate GPG key management into home-manager configuration
    Added a new module for GPG key management to automate the import of public keys from a specified directory. Updated the home-manager configuration to include the new GPG module and enabled it by default. Removed manual GPG key import instructions from README.

---
+++ modules/home-manager/gpg.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  keyDir = ../../keys;
+  getAllKeys = keyDir: builtins.attrNames (builtins.readDir keyDir);
+  isKeyAllowed = keyName: disallowedKeys: builtins.elem keyName disallowedKeys;
+  filterAllowedKeys = allKeys: disallowedKeys: builtins.filter (key: !isKeyAllowed key disallowedKeys) allKeys;
+  mapKeyPaths = keys: keyDir: builtins.map (keyName: {source = "${keyDir}/${keyName}";}) keys;
+in {
+  options.gpg = {
+    enable = lib.mkEnableOption "Enables GPG.";
+    disallowedKeys = lib.mkOption {
+      default = [];
+      description = "Keys that you don't wish to import.";
+      type = with lib.types; listOf str;
+    };
+  };
+
+  config = lib.mkIf config.gpg.enable {
+    programs.gpg = {
+      enable = true;
+
+      # Read public keys from the `keys` directory.
+      publicKeys = mapKeyPaths (filterAllowedKeys (getAllKeys keyDir) config.gpg.disallowedKeys) keyDir;
+    };
+  };
+}
