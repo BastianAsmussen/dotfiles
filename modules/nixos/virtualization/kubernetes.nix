1a7f9d69e742fcf37afac0f6f4a8ce14d870e67a
Bastian Asmussen <bastian@asmussen.tech>
2024-07-12 14:14:50 +0200

    chore: add Kubernetes master node configuration with networking and service settings

--- modules/nixos/virtualization/kubernetes.nix
+++ modules/nixos/virtualization/kubernetes.nix
@@ -3,12 +3,34 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  kubeMasterIP = "127.0.0.1";
+  kubeMasterHostname = "api.kube";
+  kubeMasterAPIServerPort = 6443;
+in {
   options.kubernetes.enable = lib.mkEnableOption "Enable Kubernetes locally.";
 
   config = lib.mkIf config.kubernetes.enable {
+    networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
     environment.systemPackages = with pkgs; [
+      kompose
+      kubectl
+      kubernetes
       minikube
     ];
+
+    services.kubernetes = {
+      roles = ["master" "node"];
+      masterAddress = kubeMasterHostname;
+      apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
+      easyCerts = true;
+      apiserver = {
+        securePort = kubeMasterAPIServerPort;
+        advertiseAddress = kubeMasterIP;
+      };
+
+      addons.dns.enable = true;
+      kubelet.extraOpts = "--fail-swap-on=false";
+    };
   };
 }
