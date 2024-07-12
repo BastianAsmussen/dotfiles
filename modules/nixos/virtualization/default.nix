350e71e766e80b26138c36499c2440011a4da002
Bastian Asmussen <bastian@asmussen.tech>
2024-07-12 11:00:51 +0200

    chore: modularize virtualization configuration and remove unused packages
    Refactored virtualization settings into separate modules for better organization and maintainability. Removed unused packages from the configuration.

---
+++ modules/nixos/virtualization/default.nix
@@ -0,0 +1,11 @@
+{lib, ...}: {
+  imports = [
+    ./docker.nix
+    ./kubernetes.nix
+    ./qemu.nix
+  ];
+
+  docker.enable = lib.mkDefault true;
+  kubernetes.enable = lib.mkDefault true;
+  qemu.enable = lib.mkDefault true;
+}
