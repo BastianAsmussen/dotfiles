0447c02cb88a06382b4ab990a168f06d22ad5fa0
Bastian Asmussen <bastian@asmussen.tech>
2024-07-20 08:33:56 +0200

    chore: update git configuration to improve branch management and setup

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -12,7 +12,15 @@
     };
 
     extraConfig = {
-      pull.rebase = true;
+      push.autoSetupRemote = true;
+      fetch.prune = true; # Automatically delete dead branches.
+      pull = {
+        ff = false;
+        commit = false;
+        rebase = true;
+        prune = true; # Automatically delete dead branches.
+      };
+
       init.defaultBranch = "master";
     };
   };
