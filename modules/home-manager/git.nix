cff7eca626b13165cf352a627dbc42ed33b1bbb5
Bastian Asmussen <bastian@asmussen.tech>
2024-07-21 12:20:17 +0200

    chore: enhance git configuration with additional settings and alias

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -11,15 +11,27 @@
       signByDefault = true;
     };
 
+    aliases.staash = "stash --all";
+
     extraConfig = {
       push.autoSetupRemote = true;
-      fetch.prune = true; # Automatically delete dead branches.
+
+      fetch = {
+        prune = true; # Automatically delete dead branches.
+        writeCommitGraph = true; # Incrementally build commit graph.
+      };
+
       pull = {
         ff = false;
         commit = false;
         rebase = true;
         prune = true; # Automatically delete dead branches.
       };
+
+      rerere.enabled = true; # Automatically resolve merge conflicts if they've been seen before.
+
+      column.ui = "auto"; # Make `git branch` prettier.
+      branch.sort = "-committerdate"; # Sort by most recent commit.
 
       init.defaultBranch = "master";
     };
