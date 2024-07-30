fde70fabee5e31db526142ec4dd86904f454cd49
Bastian Asmussen <bastian@asmussen.tech>
2024-07-30 23:23:13 +0200

    feat: add Catppuccin Mocha Lavender theme to Firefox extensions and enable auto-disable scopes

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -1,9 +1,24 @@
 {
-  pkgs,
   inputs,
+  pkgs,
+  lib,
   username,
   ...
-}: {
+}: let
+  catppuccin-mocha-lavender-git = inputs.firefox-addons.lib.${pkgs.system}.buildFirefoxXpiAddon rec {
+    pname = "catppuccin-mocha-lavender-git";
+    version = "2.0";
+    addonId = "{8446b178-c865-4f5c-8ccc-1d7887811ae3}";
+    url = "https://addons.mozilla.org/firefox/downloads/file/3990315/catppuccin_mocha_lavender_git-${version}.xpi";
+    sha256 = "sha256-cCkrC4ZSy6tAjRXSYdxRUPaQ+1u6+W9OcxclbH2beTM=";
+    meta = with lib; {
+      homepage = "https://github.com/catppuccin/firefox";
+      description = "🦊 Soothing pastel theme for Firefox";
+      license = licenses.mit;
+      platforms = platforms.all;
+    };
+  };
+in {
   programs.firefox = {
     enable = true;
 
@@ -59,6 +74,9 @@
         "extensions.pocket.enabled" = false;
         "privacy.globalprivacycontrol.enabled" = true;
         "privacy.donottrackheader.enabled" = true;
+
+        # Auto-enable extensions.
+        "extensions.autoDisableScopes" = 0;
       };
 
       bookmarks = [
@@ -72,17 +90,19 @@
         }
       ];
 
-      extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
-        bitwarden
-        ublock-origin
-        clearurls
-        duckduckgo-privacy-essentials
-        sponsorblock
-        return-youtube-dislikes
-        darkreader
-        wayback-machine
-        i-dont-care-about-cookies
-      ];
+      extensions =
+        (with inputs.firefox-addons.packages.${pkgs.system}; [
+          bitwarden
+          ublock-origin
+          clearurls
+          duckduckgo-privacy-essentials
+          sponsorblock
+          return-youtube-dislikes
+          darkreader
+          wayback-machine
+          i-dont-care-about-cookies
+        ])
+        ++ [catppuccin-mocha-lavender-git];
     };
   };
 }
