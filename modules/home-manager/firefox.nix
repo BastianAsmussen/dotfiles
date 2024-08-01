03656ea6c27d35af89f415a42b79a327317242e7
Bastian Asmussen <bastian@asmussen.tech>
2024-08-01 13:31:31 +0200

    chore: enhance Firefox configuration with additional privacy settings and bookmarks
    Expanded privacy settings and bookmarks for improved user experience and security. Removed redundant code and streamlined extension management.

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -1,30 +1,35 @@
 {
   inputs,
   pkgs,
-  lib,
   username,
   ...
-}: let
-  catppuccin-mocha-lavender-git = inputs.firefox-addons.lib.${pkgs.system}.buildFirefoxXpiAddon rec {
-    pname = "catppuccin-mocha-lavender-git";
-    version = "2.0";
-    addonId = "{8446b178-c865-4f5c-8ccc-1d7887811ae3}";
-    url = "https://addons.mozilla.org/firefox/downloads/file/3990315/catppuccin_mocha_lavender_git-${version}.xpi";
-    sha256 = "sha256-cCkrC4ZSy6tAjRXSYdxRUPaQ+1u6+W9OcxclbH2beTM=";
-    meta = with lib; {
-      homepage = "https://github.com/catppuccin/firefox";
-      description = "🦊 Soothing pastel theme for Firefox";
-      license = licenses.mit;
-      platforms = platforms.all;
-    };
-  };
-in {
+}: {
   programs.firefox = {
     enable = true;
 
     policies = {
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+      };
+      ExtensionUpdate = false;
+      ExtensionSettings."{8446b178-c865-4f5c-8ccc-1d7887811ae3}" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/file/3990315/catppuccin_mocha_lavender_git-2.0.xpi";
+        installation_mode = "force_installed";
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = "always";
     };
 
     profiles.${username} = {
@@ -69,40 +74,75 @@ in {
       };
 
       settings = {
-        "signon.rememberSignons" = false;
-        "identity.fxaccounts.enabled" = false;
-        "extensions.pocket.enabled" = false;
-        "privacy.globalprivacycontrol.enabled" = true;
-        "privacy.donottrackheader.enabled" = true;
+        "signon.rememberSignons" = false; # Disable password saving.
 
         # Auto-enable extensions.
         "extensions.autoDisableScopes" = 0;
+
+        # Telemetry.
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.ping-centre.telemetry" = false;
+        "browser.tabs.crashReporting.sendReport" = false;
+        "devtools.onboarding.telemetry.logged" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.server" = "data:,";
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "extensions.webcompat-reporter.enabled" = false;
+        "datareporting.policy.dataSubmissionEnabled" = false;
+        "datareporting.healthreport.uploadEnabled" = false;
+        "browser.urlbar.eventTelemetry.enabled" = false;
+
+        # Pocket Extension.
+        "browser.newtabpage.activity-stream.feeds.discoverystreamfeed" = false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+
+        # Privacy.
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+        "privacy.userContext.enabled" = true;
+        "privacy.userContext.ui.enabled" = true;
+        "browser.send_pings" = false;
+        "app.normandy.enabled" = false;
+        "app.shield.optoutstudies.enabled" = false;
       };
 
       bookmarks = [
         {
-          name = "Mail";
-          url = "https://mail.proton.me/u/0/inbox";
-        }
-        {
-          name = "GitHub";
-          url = "https://github.com";
+          toolbar = true;
+          bookmarks = [
+            {
+              name = "Mail";
+              keyword = "ma";
+              url = "https://mail.proton.me/u/0";
+            }
+            {
+              name = "GitHub";
+              keyword = "gh";
+              url = "https://github.com";
+            }
+          ];
         }
       ];
 
-      extensions =
-        (with inputs.firefox-addons.packages.${pkgs.system}; [
-          bitwarden
-          ublock-origin
-          clearurls
-          duckduckgo-privacy-essentials
-          sponsorblock
-          return-youtube-dislikes
-          darkreader
-          wayback-machine
-          i-dont-care-about-cookies
-        ])
-        ++ [catppuccin-mocha-lavender-git];
+      extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+        bitwarden
+        ublock-origin
+        clearurls
+        duckduckgo-privacy-essentials
+        sponsorblock
+        return-youtube-dislikes
+        darkreader
+        i-dont-care-about-cookies
+      ];
     };
   };
 }
