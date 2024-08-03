91f18c4964d1a4ceb639d3976543e74902415d3c
Bastian Asmussen <bastian@asmussen.tech>
2024-08-03 10:35:07 +0200

    chore: update Neovim configuration for better usability and performance

--- modules/home-manager/nixvim/default.nix
+++ modules/home-manager/nixvim/default.nix
@@ -17,9 +17,28 @@
       number = true;
       relativenumber = true;
 
+      tabstop = 4;
+      softtabstop = 4;
       shiftwidth = 4;
+      expandtab = true;
 
-      updatetime = 100; # Faster completion.
+      smartindent = true;
+
+      wrap = false;
+
+      swapfile = false;
+      backup = false;
+      undofile = true;
+
+      hlsearch = false;
+      incsearch = true;
+
+      termguicolors = true;
+
+      scrolloff = 8; # Number of lines to show around the cursor.
+
+      updatetime = 50; # Faster completion.
+      colorcolumn = "80";
     };
 
     globals = {
