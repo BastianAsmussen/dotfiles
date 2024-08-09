4ecae8933e1b589679278e1da21262aca948ede9
Bastian Asmussen <bastian@asmussen.tech>
2024-08-09 11:30:58 +0200

    chore: update password-store module to include PASSWORD_STORE_DIR setting for user-specific directory configuration

--- modules/home-manager/password-store.nix
+++ modules/home-manager/password-store.nix
@@ -1,6 +1,12 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  hmOptions,
+  ...
+}: {
   programs.password-store = {
     enable = true;
     package = pkgs.pass.withExtensions (exts: with exts; [pass-import pass-otp]);
+
+    settings.PASSWORD_STORE_DIR = "/home/${hmOptions.username}/.password-store";
   };
 }
