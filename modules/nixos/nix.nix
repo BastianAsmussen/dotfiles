cea9bf533e70b101efb9419a5f52772ae5fef809
Bastian Asmussen <bastian@asmussen.tech>
2024-08-13 20:44:46 +0200

    chore: update nix configuration to include extra trusted public keys and substituters for devenv cache

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -6,7 +6,9 @@
       experimental-features = ["nix-command" "flakes"];
 
       trusted-users = ["root" "@wheel"];
-      substituters = ["https://cache.nixos.org" "https://devenv.cachix.org"];
+
+      extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+      extra-substituters = "https://devenv.cachix.org";
 
       warn-dirty = false;
       auto-optimise-store = true;
