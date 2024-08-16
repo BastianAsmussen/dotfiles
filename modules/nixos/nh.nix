0d47d0934f6272f0cce9c80bb8e432bb639f50a4
Bastian Asmussen <bastian@asmussen.tech>
2024-08-16 09:59:08 +0200

    chore: update nh configuration to adjust clean-up parameters and specify flake path

--- modules/nixos/nh.nix
+++ modules/nixos/nh.nix
@@ -1,11 +1,13 @@
-{
+{config, ...}: {
   programs.nh = {
     enable = true;
 
     clean = {
       enable = true;
 
-      extraArgs = "--delete-older-than 7d";
+      extraArgs = "--delete-older-than 4d --keep 3";
     };
+
+    flake = "/home/${config.home-manager.username}/dotfiles";
   };
 }
