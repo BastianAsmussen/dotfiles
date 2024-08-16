f9d51670e3351ca773240ac71b5622b10b903f92
Bastian Asmussen <bastian@asmussen.tech>
2024-08-16 10:13:39 +0200

    fix: correct RUSTFLAGS specification

--- modules/home-manager/nixvim/plugins/lsp/rustaceanvim.nix
+++ modules/home-manager/nixvim/plugins/lsp/rustaceanvim.nix
@@ -1,21 +1,18 @@
-{lib, ...}: {
+{
   programs.nixvim.plugins.rustaceanvim = {
     enable = true;
 
-    settings.server = {
-      auto_attach = true;
+    settings.server.default_settings.rust-analyzer = {
+      cargo.extraEnv.RUSTFLAGS = builtins.concatStringsSep " " [
+        "-Dclippy::enum_glob_use" # Disallow use of `use` of all enums.
+        "-Dclippy::pedantic" # Enable all pedantic lints.
+        "-Dclippy::nursery" # Enable all nursery lints.
+        "-Dclippy::unwrap_used" # Disallow use of `unwrap`.
+      ];
+      check.command = "clippy";
 
-      default_settings.rust-analyzer = {
-        cargo.extraEnv.RUSTFLAGS = lib.strings.concatStrings [
-          "-Dclippy::enum_glob_use" # Disallow use of `use` of all enums.
-          "-Dclippy::pedantic" # Enable all pedantic lints.
-          "-Dclippy::nursery" # Enable all nursery lints.
-          "-Dclippy::unwrap_used" # Disallow use of `unwrap`.
-        ];
-
-        diagnostics.styleLints.enable = true;
-        files.excludeDirs = [".devenv"];
-      };
+      diagnostics.styleLints.enable = true;
+      files.excludeDirs = [".devenv"];
     };
   };
 }
