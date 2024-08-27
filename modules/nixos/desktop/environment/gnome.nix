9f8774fc19025d08301d3cb0f7f351e1d663b4e9
Bastian Asmussen <bastian@asmussen.tech>
2024-08-27 07:31:36 +0200

    chore: standardize GNOME capitalization in option description

--- modules/nixos/desktop/environment/gnome.nix
+++ modules/nixos/desktop/environment/gnome.nix
@@ -3,7 +3,7 @@
   config,
   ...
 }: {
-  options.desktop.environment.gnome.enable = lib.mkEnableOption "Enables the `Gnome` desktop environment.";
+  options.desktop.environment.gnome.enable = lib.mkEnableOption "Enables the `GNOME` desktop environment.";
 
   config = lib.mkIf config.desktop.environment.gnome.enable {
     services.xserver.desktopManager.gnome.enable = true;
