a6d77825bebfbeafe1a838b250a835f4efd13d68
Bastian Asmussen <bastian@asmussen.tech>
2024-09-12 07:36:57 +0200

    feat: migrate to Hyprland with AGS
    - I'm borrowing a lot from https://github.com/Aylur/dotfiles. Thank you, Aylur! :)
    - Update flake.lock dependencies.

--- modules/home-manager/desktop/hyprland/default.nix
+++ modules/home-manager/desktop/hyprland/default.nix
@@ -6,31 +6,17 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkOption types mkIf mkForce;
-  inherit (builtins) concatLists genList toString;
+  inherit (lib) mkIf mkForce;
+  inherit (builtins) genList toString;
 
-  cfg = {
-    inherit (config) hyprland;
-    inherit (config.lib.stylix) colors;
-  };
-
-  startupScript = pkgs.writers.writeBashBin "start" ''
-    ags &
-  '';
+  playerctl = "${pkgs.playerctl}/bin/playerctl";
+  brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+  pactl = "${pkgs.pulseaudio}/bin/pactl";
+  screenshot = import ./scripts/screenshot.nix pkgs;
 in {
   imports = [
     ./ags
   ];
-
-  options.hyprland.monitors = mkOption {
-    default = [
-      "DP-1, 1920x1080@240, 0x0, 1"
-      "HDMI-A-1, 1920x1080, -1920x0, 1"
-      ", preferred, auto, 1" # Recommended rule for quickly plugging in random monitors.
-    ];
-    description = "The monitors to use for Hyprland.";
-    type = types.listOf types.str;
-  };
 
   config = mkIf nixosConfig.desktop.environment.hyprland.enable {
     wayland.windowManager.hyprland = {
@@ -42,80 +28,209 @@ in {
       systemd.enableXdgAutostart = true;
 
       settings = {
-        exec-once = "${startupScript}/bin/start";
+        exec-once = [
+          "ags -b hypr"
+          "fragments"
+        ];
 
         "$mod" = "SUPER";
-        "$terminal" = "alacritty";
-        "$browser" = "firefox";
 
         input = {
           kb_layout = "dk";
-
           follow_mouse = 1;
-          touchpad.natural_scroll = true;
+          touchpad = {
+            natural_scroll = "yes";
+            disable_while_typing = true;
+            drag_lock = true;
+          };
+
+          sensitivity = 0;
+          float_switch_override_focus = 2;
         };
 
-        monitor = cfg.hyprland.monitors;
-        bind =
+        monitor = [
+          ",preferred,auto,1"
+        ];
+
+        dwindle = {
+          pseudotile = "yes";
+          preserve_split = "yes";
+        };
+
+        gestures = {
+          workspace_swipe = true;
+          workspace_swipe_use_r = true;
+        };
+
+        misc = {
+          disable_splash_rendering = true;
+          force_default_wallpaper = 1;
+        };
+
+        windowrule = let
+          mkFloating = regex: "float, ^(${regex})$";
+        in [
+          (mkFloating "org.gnome.Calculator")
+          (mkFloating "org.gnome.Nautilus")
+          (mkFloating "pavucontrol")
+          (mkFloating "nm-connection-editor")
+          (mkFloating "org.gnome.Settings")
+          (mkFloating "org.gnome.design.Palette")
+          (mkFloating "Color Picker")
+          (mkFloating "xdg-desktop-portal")
+          (mkFloating "xdg-desktop-portal-gnome")
+          (mkFloating "de.haeckerfelix.Fragments")
+          (mkFloating "com.github.Aylur.ags")
+        ];
+
+        binds.allow_workspace_cycles = true;
+        bind = let
+          mkBind = mod: cmd: key: arg: "${mod}, ${key}, ${cmd}, ${arg}";
+          mkWorkspaceBind = mkBind "$mod" "workspace";
+          mkMoveFocusBind = mkBind "$mod" "movefocus";
+          mkResizeActiveBind = mkBind "$mod CTRL" "resizeactive";
+          mkMoveActiveBind = mkBind "$mod ALT" "moveactive";
+          mkMoveToWorkspaceBind = mkBind "$mod SHIFT" "movetoworkspace";
+
+          workspaceList = genList (x: x + 1) 9;
+
+          ags = "exec, ags -b hypr";
+        in
           [
-            "$mod, RETURN, exec, $terminal"
-            "$mod, F, exec, $browser"
-            "$mod, SPACE, exec, ${pkgs.rofi-wayland}/bin/rofi -show drun -show-icons"
+            "CTRL SHIFT, r, ${ags} quit; ags -b hypr"
 
-            "$mod, Q, killactive"
-            "$mod, M, exit"
-            "$mod SHIFT, F, fullscreen, 1"
-            "$mod, BACKSPACE, exec, ${pkgs.swaylock}/bin/swaylock"
+            "$mod, r, ${ags} -t launcher"
+            "$mod, Tab, ${ags} -t overview"
 
-            "$mod, H, movefocus, l"
-            "$mod, L, movefocus, r"
-            "$mod, K, movefocus, u"
-            "$mod, J, movefocus, d"
+            ",XF86PowerOff, ${ags} -r 'powermenu.shutdown()'"
+
+            ",Print, exec, ${screenshot}"
+            "SHIFT, Print, exec, ${screenshot} --full"
+
+            "$mod, Return, exec, alacritty"
+            "$mod, w, exec, firefox"
+
+            "ALT, Tab, focuscurrentorlast"
+            "CTRL ALT, Delete, exit"
+            "ALT, q, killactive"
+
+            "$mod, f, togglefloating"
+            "$mod, g, fullscreen"
+            "$mod, o, fullscreen, 0"
+            "$mod, p, togglesplit"
+
+            (mkMoveFocusBind "h" "l")
+            (mkMoveFocusBind "j" "d")
+            (mkMoveFocusBind "k" "u")
+            (mkMoveFocusBind "l" "r")
+
+            (mkWorkspaceBind "left" "e-1")
+            (mkWorkspaceBind "right" "e+1")
+
+            (mkResizeActiveBind "h" "-20 0")
+            (mkResizeActiveBind "j" "0 20")
+            (mkResizeActiveBind "k" "0 -20")
+            (mkResizeActiveBind "l" "20 0")
+
+            (mkMoveActiveBind "h" "-20 0")
+            (mkMoveActiveBind "j" "0 20")
+            (mkMoveActiveBind "k" "0 -20")
+            (mkMoveActiveBind "l" "20 0")
           ]
-          ++ (
-            concatLists (genList (
-                x: let
-                  ws = let
-                    c = (x + 1) / 10;
-                  in
-                    toString (x + 1 - (c * 10));
-                in [
-                  "$mod, ${ws}, workspace, ${toString (x + 1)}"
-                  "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-                ]
-              )
-              10)
-          );
+          ++ (map (i: mkWorkspaceBind (toString i) (toString i)) workspaceList)
+          ++ (map (i: mkMoveToWorkspaceBind (toString i) (toString i)) workspaceList);
+
+        bindle = [
+          ",XF86MonBrightnessUp, exec, ${brightnessctl} set +5%"
+          ",XF86MonBrightnessDown, exec, ${brightnessctl} set 5%-"
+          ",XF86KbdBrightnessUp, exec, ${brightnessctl} -d asus::kbd_backlight set +1"
+          ",XF86KbdBrightnessDown, exec, ${brightnessctl} -d asus::kbd_backlight set  1-"
+          ",XF86AudioRaiseVolume, exec, ${pactl} set-sink-volume @DEFAULT_SINK@ +5%"
+          ",XF86AudioLowerVolume, exec, ${pactl} set-sink-volume @DEFAULT_SINK@ -5%"
+        ];
+
+        bindl = [
+          ",XF86AudioPlay, exec, ${playerctl} play-pause"
+          ",XF86AudioStop, exec, ${playerctl} pause"
+          ",XF86AudioPause, exec, ${playerctl} pause"
+          ",XF86AudioPrev, exec, ${playerctl} previous"
+          ",XF86AudioNext, exec, ${playerctl} next"
+          ",XF86AudioMicMute, exec, ${pactl} set-source-mute @DEFAULT_SOURCE@ toggle"
+        ];
 
         bindm = [
           "$mod, mouse:272, movewindow"
           "$mod, mouse:273, resizewindow"
-          "$mod ALT, mouse:272, resizewindow"
         ];
 
         general = {
-          "col.active_border" = mkForce "rgba(${cfg.colors.base0E}ff) rgba(${cfg.colors.base09}ff) 60deg";
-          "col.inactive_border" = mkForce "rgba(${cfg.colors.base00}ff)";
-
-          border_size = 2;
-          gaps_out = 28;
+          layout = "dwindle";
+          resize_on_border = true;
         };
 
-        decoration.rounding = 12;
+        decoration = {
+          rounding = 12;
+
+          drop_shadow = "yes";
+          shadow_range = 8;
+          shadow_render_power = 2;
+          "col.shadow" = mkForce "rgba(00000044)";
+
+          dim_inactive = false;
+
+          blur = {
+            enabled = true;
+            size = 8;
+            passes = 3;
+            new_optimizations = "on";
+            noise = 0.01;
+            contrast = 0.9;
+            brightness = 0.8;
+            popups = true;
+          };
+        };
+
+        animations = {
+          enabled = "yes";
+          bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+          animation = [
+            "windows, 1, 5, myBezier"
+            "windowsOut, 1, 7, default, popin 80%"
+            "border, 1, 10, default"
+            "fade, 1, 7, default"
+            "workspaces, 1, 6, default"
+          ];
+        };
+
+        plugin = {
+          overview = {
+            centerAligned = true;
+            hideTopLayers = true;
+            hideOverlayLayers = true;
+            showNewWorkspace = true;
+            exitOnClick = true;
+            exitOnSwitch = true;
+            drawActiveWorkspace = true;
+            reverseSwipe = true;
+          };
+
+          hyprbars = {
+            bar_color = "rgb(2a2a2a)";
+            bar_height = 28;
+            col_text = "rgba(ffffffdd)";
+            bar_text_size = 11;
+            bar_text_font = "Ubuntu Nerd Font";
+
+            buttons = {
+              button_size = 0;
+              "col.maximize" = "rgba(ffffff11)";
+              "col.close" = "rgba(ff111133)";
+            };
+          };
+        };
       };
 
       extraConfig = ''
-        bezier = easeOutBack, 0.34, 1.56, 0.64, 1
-        bezier = easeInBack, 0.36, 0, 0.66, -0.56
-        bezier = easeInCubic, 0.32, 0, 0.67 ,0
-        bezier = easeInOutCubic, 0.65, 0, 0.35, 1
-
-        animation = windowsIn, 1, 5, easeOutBack, popin
-        animation = windowsOut, 1, 5, easeInBack, popin
-        animation = fadeIn, 0
-        animation = fadeOut, 1, 10, easeInCubic
-        animation = workspaces, 1, 4, easeInOutCubic, slide
-
         xwayland:force_zero_scaling = true
       '';
     };
