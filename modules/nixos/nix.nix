565cf37e5e8d538bbb6a28dbaefd205f3342b61f
Bastian Asmussen <bastian@asmussen.tech>
2024-10-09 08:05:35 +0200

    chore: remove redundant Nix optimize timer since we optimize on every build anyways

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,17 +1,15 @@
 {
   nixpkgs.config.allowUnfree = true;
-  nix = {
-    settings = {
-      experimental-features = ["nix-command" "flakes"];
-      trusted-users = ["root" "@wheel"];
-      extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-      extra-substituters = "https://devenv.cachix.org";
-      warn-dirty = false;
-      auto-optimise-store = true;
-      http-connections = 64;
-      log-lines = 64;
-    };
-
-    optimise.automatic = true;
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    trusted-users = ["root" "@wheel"];
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+    warn-dirty = false;
+    auto-optimise-store = true;
+    http-connections = 64;
+    log-lines = 64;
+    keep-outputs = true;
+    keep-derivations = true;
   };
 }
