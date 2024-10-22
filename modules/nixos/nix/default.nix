9871eb8eda35e0b14362aba3f7622d570078a9d9
Bastian Asmussen <bastian@asmussen.tech>
2024-10-23 00:37:51 +0200

    chore: enhance Nix configuration with additional settings and inputs
    This update introduces new settings to the Nix configuration, including registry and nixPath mappings for flake inputs, connection timeout, and memory management settings. It also reorganizes existing settings for better clarity and maintainability.

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -1,4 +1,12 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  inputs,
+  config,
+  ...
+}: let
+  mibToB = mib: mib * 1024 * 1024;
+in {
   imports = [
     ./nh.nix
   ];
@@ -6,32 +14,39 @@
   nix = {
     package = pkgs.lix;
 
+    # Add each flake input as a registry.
+    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+    # Add inputs to the system's legacy channels.
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
     settings = {
       experimental-features = ["nix-command" "flakes" "recursive-nix" "ca-derivations"];
       trusted-users = ["root" "@wheel"];
-
-      extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-      extra-substituters = "https://devenv.cachix.org";
       flake-registry = "/etc/nix/registry.json";
 
+      connect-timeout = 5; # Timeout after 5 seconds.
       max-jobs = "auto";
-      warn-dirty = false;
       auto-optimise-store = true;
       builders-use-substitutes = true;
+      fallback = true; # Fallback to building from source if binary substitute fails.
       keep-going = true;
       keep-outputs = true;
       keep-derivations = true;
       accept-flake-config = true;
+      warn-dirty = false;
       commit-lockfile-summary = "chore: update flake.lock";
+      min-free = mibToB 128;
+      max-free = mibToB 1024;
+
+      extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+      extra-substituters = "https://devenv.cachix.org";
     };
   };
 
   nixpkgs.config.allowUnfree = true;
 
   # Don't build on tmpfs, it's not a good idea.
-  systemd.services.nix-daemon = {
-    environment.TMPDIR = "/var/tmp";
-  };
+  systemd.services.nix-daemon.environment.TMPDIR = "/var/tmp";
 
   # Use the Rust rewrite of `switch` instead of the original one.
   system.switch = {
