8455a28a7e14f72e6c392d3eb9b3758a6dc3ca60
Bastian Asmussen <bastian@asmussen.tech>
2024-11-04 17:46:50 +0100

    feat: introduce weak-tap-time for improved key responsiveness

--- modules/nixos/keyboard.nix
+++ modules/nixos/keyboard.nix
@@ -22,6 +22,7 @@
 
           (defvar
             tap-time 200
+            weak-tap-time 250
             hold-time 150
 
             left-hand-keys (
@@ -62,15 +63,15 @@
               (on-idle-fakekey to-base tap 20) ;; If we stop typing for 20ms, then switch back to the base layer.
             )
 
-            a (tap-hold-release-keys $tap-time $hold-time (multi a @tap) lmet $left-hand-keys)
-            s (tap-hold-release-keys $tap-time $hold-time (multi s @tap) lalt $left-hand-keys)
+            a (tap-hold-release-keys $weak-tap-time $hold-time (multi a @tap) lmet $left-hand-keys)
+            s (tap-hold-release-keys $weak-tap-time $hold-time (multi s @tap) lalt $left-hand-keys)
             d (tap-hold-release-keys $tap-time $hold-time (multi d @tap) lctl $left-hand-keys)
             f (tap-hold-release-keys $tap-time $hold-time (multi f @tap) lsft $left-hand-keys)
 
             j (tap-hold-release-keys $tap-time $hold-time (multi j @tap) rsft $right-hand-keys)
             k (tap-hold-release-keys $tap-time $hold-time (multi k @tap) rctl $right-hand-keys)
-            l (tap-hold-release-keys $tap-time $hold-time (multi l @tap) ralt $right-hand-keys)
-            ; (tap-hold-release-keys $tap-time $hold-time (multi ; @tap) rmet $right-hand-keys)
+            l (tap-hold-release-keys $weak-tap-time $hold-time (multi l @tap) ralt $right-hand-keys)
+            ; (tap-hold-release-keys $weak-tap-time $hold-time (multi ; @tap) rmet $right-hand-keys)
           )
         '';
       };
