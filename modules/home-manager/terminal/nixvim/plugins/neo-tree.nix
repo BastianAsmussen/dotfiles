d51f898df71bd6a4af29ff0d2e9829b0ce485c58
BastianAsmussen <BastianAsmussen@users.noreply.github.com>
2024-11-13 13:22:32 +0000

    chore: run auto formatting and linting tools

--- modules/home-manager/terminal/nixvim/plugins/neo-tree.nix
+++ modules/home-manager/terminal/nixvim/plugins/neo-tree.nix
@@ -7,40 +7,40 @@
     };
 
     plugins.neo-tree = {
-    enable = true;
+      enable = true;
 
-    enableGitStatus = true;
-    enableModifiedMarkers = true;
-    enableRefreshOnWrite = true;
-    sources = [
-      "filesystem"
-      "buffers"
-      "git_status"
-      "document_symbols"
-    ];
+      enableGitStatus = true;
+      enableModifiedMarkers = true;
+      enableRefreshOnWrite = true;
+      sources = [
+        "filesystem"
+        "buffers"
+        "git_status"
+        "document_symbols"
+      ];
 
-    addBlankLineAtTop = false;
-    hideRootNode = true;
-    filesystem = {
-      followCurrentFile.enabled = true;
-      filteredItems.hideDotfiles = false;
-    };
+      addBlankLineAtTop = false;
+      hideRootNode = true;
+      filesystem = {
+        followCurrentFile.enabled = true;
+        filteredItems.hideDotfiles = false;
+      };
 
-    window.width = 32;
-    defaultComponentConfigs = {
-      indent.withExpanders = true;
-      gitStatus.symbols = {
-        added = " ";
-        conflict = "󰩌 ";
-        deleted = "󱂥";
-        ignored = " ";
-        modified = " ";
-        renamed = "󰑕";
-        staged = "󰩍";
-        unstaged = "";
-        untracked = "";
+      window.width = 32;
+      defaultComponentConfigs = {
+        indent.withExpanders = true;
+        gitStatus.symbols = {
+          added = " ";
+          conflict = "󰩌 ";
+          deleted = "󱂥";
+          ignored = " ";
+          modified = " ";
+          renamed = "󰑕";
+          staged = "󰩍";
+          unstaged = "";
+          untracked = "";
+        };
       };
     };
   };
-};
 }
