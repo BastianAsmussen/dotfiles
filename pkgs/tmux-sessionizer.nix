6ef2ae35a82f89907da028a970e19620adecb3f1
Bastian Asmussen <bastian@asmussen.tech>
2024-11-15 23:35:26 +0100

    refactor: update attributes of tmux-sessionizer.nix

--- pkgs/tmux-sessionizer.nix
+++ pkgs/tmux-sessionizer.nix
@@ -1,23 +1,28 @@
 {
-  pkgs,
+  stdenv,
+  fetchFromGitHub,
+  bash,
+  makeWrapper,
   lib,
+  tmux,
+  fzf,
 }:
-pkgs.stdenv.mkDerivation {
+stdenv.mkDerivation {
   name = "tmux-sessionizer";
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "BastianAsmussen";
     repo = "tmux-sessionizer";
     rev = "c0777f69faed032fa7351a881e86ed37ac4d6b37";
     hash = "sha256-byMOiXvCh0XO9W4klfJIsVEql+bMcxwf39I7+nTb8yM=";
   };
 
-  buildInputs = [pkgs.bash];
-  nativeBuildInputs = [pkgs.makeWrapper];
+  buildInputs = [bash];
+  nativeBuildInputs = [makeWrapper];
   installPhase = ''
     mkdir -p $out/bin
     cp tmux-sessionizer $out/bin/tmux-sessionizer
     wrapProgram $out/bin/tmux-sessionizer \
-      --prefix PATH : ${lib.makeBinPath (with pkgs; [tmux fzf])}
+      --prefix PATH : ${lib.makeBinPath [tmux fzf]}
   '';
 
   meta = {
