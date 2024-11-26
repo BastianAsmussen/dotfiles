ad5234de1f94f3162360dcd14f976a30d77a04fe
Bastian Asmussen <bastian@asmussen.tech>
2024-11-26 09:29:16 +0100

    feat: enhance distrobox configuration with default settings
    Add default container settings to improve user experience by setting default image, name, and enabling non-interactive mode.

--- modules/home-manager/terminal/distrobox.nix
+++ modules/home-manager/terminal/distrobox.nix
@@ -3,5 +3,9 @@
 
   xdg.configFile."distrobox/distrobox.conf".text = ''
     container_additional_volumes="/nix/store:/nix/store:ro /etc/static/profiles/per-user:/etc/profiles/per-user:ro"
+    container_always_pull="1"
+    container_image_default="archlinux:latest"
+    container_name_default="archlinux"
+    non_interactive="1"
   '';
 }
