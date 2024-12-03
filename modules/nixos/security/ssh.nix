0db52fa20e589cac3d4dc73dfebe4011a204c2e2
Bastian Asmussen <bastian@asmussen.tech>
2024-12-03 07:27:51 +0100

    chore: enhance SSH security and user configuration
    Improved SSH security by adding fail2ban settings to mitigate brute force attacks and updated SSH server settings for stricter access control. Enhanced user configuration by including authorized SSH keys for secure access.

--- modules/nixos/security/ssh.nix
+++ modules/nixos/security/ssh.nix
@@ -6,13 +6,46 @@
   options.ssh.server.enable = lib.mkEnableOption "Enable SSH server.";
 
   config = lib.mkIf config.ssh.server.enable {
-    services.openssh = {
-      enable = true;
+    services = {
+      openssh = {
+        enable = true;
 
-      settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-        PermitRootLogin = "no";
+        settings = {
+          UseDns = true;
+          PasswordAuthentication = false;
+          KbdInteractiveAuthentication = false;
+          PermitRootLogin = "no";
+          MaxAuthTries = 3;
+          LoginGraceTime = 20;
+          ChallengeResponseAuthentication = "no";
+          PermitUserEnvironment = "no";
+          AllowAgentForwarding = "no";
+          AllowTcpForwarding = "no";
+          PermitTunnel = "no";
+        };
+      };
+
+      fail2ban = let
+        minsToSecs = mins: mins * 60;
+      in {
+        enable = true;
+
+        maxretry = 5; # Ban IP after 5 failures.
+        bantime = "24h"; # Ban IPs for one day on the first ban.
+        bantime-increment = {
+          enable = true; # Enable increment of bantime after each violation.
+          multipliers = "1 2 4 8 16 32 64";
+          maxtime = "168h"; # Do not ban for more than 1 week.
+          overalljails = true; # Calculate the bantime based on all the violations.
+        };
+
+        jails.sshd.settings = {
+          port = "ssh";
+          logpath = "/var/log/auth.log";
+          maxretry = 5;
+          bantime = minsToSecs 30;
+          ignoreip = "127.0.0.1/8";
+        };
       };
     };
   };
--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -13,6 +13,9 @@ in {
     initialPassword = "Password123!";
     extraGroups = ["wheel"];
     shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC6tLYigg7LbophUDTyIp1QtrduZJFAhxqcKrn+bfvZpSw7cezQYupjPxSANJB5ujblc35UDfJXl+Bc9exzIugmYdQ4GnxQCmarc/I+l+P/MPRl0K2vAzxA/ifcecHAjBen9Nu6sSA2HxxiQ+x+7pS+Q15wUDji5pz9ENCd7G1veLpoEg+ZKtQDsPHuMGDnAAHdHRQXxAt6TDVaIYjxTjNCHYwczKYaGYJEckYnJNwqIcTMOvmI2h0mzhBVjMAYA9uqr7cmoJHwD+v3/78o1wVdQcNI5EW7t0LmDs0wLCN5cKNUWMKr7DuP3xLe+cJXgiNyHqBVoRq2FFraIktt0++KgZ/p+F/uNSbQ+ZNoW8Usragnhz4hvNDZ1nJrEt0DjFeU4lhpczvJrMYi3cmzjMSewBWZtZK9eccIjlRLPfqmt6jO0hAOBi8n0aXK4jmpG4CBfnvCZ8nW7YwTuVlzQRHgNZFs6FUcxx/yXQTHnTnQ9jT7ucoVpKQuP73x6vNIc/HFK5G3BMXsuBuz3DdEyKYhEbtlrfrXWQmX8Yp7Axglw2JMrukGIxVaMtN3vJC9VQlZMe0Lk8mBtEa0Ny9S3MSLeIL6DO39VSnx07WNHB7donYam2CP92OrIYVT2aBJkSWETDlBI4Ku6p7eZQj7TRhhWR0FMYkTVI+eJ9z/gIHOrw== cardno:26_268_691"
+    ];
   };
 
   # Set the user's icon.
