6988f1405ac4132d58cf369049f6db3f65fd62ee
Bastian Asmussen <bastian@asmussen.tech>
2024-12-05 14:49:48 +0100

    feat: enhance YubiKey support with additional tools
    Added yubioath-flutter to system packages to expand YubiKey functionality.

--- modules/nixos/security/yubiKey.nix
+++ modules/nixos/security/yubiKey.nix
@@ -9,18 +9,25 @@ in {
   options.yubiKey.enable = lib.mkEnableOption "Enables YubiKey support.";
 
   config = lib.mkIf (cfg.enable && config.gpg.enable) {
-    programs.ssh.startAgent = false; # Disallow the SSH agent.
     services = {
       udev.packages = [pkgs.yubikey-personalization];
       pcscd.enable = true;
     };
 
+    programs.ssh.startAgent = false; # Disallow the SSH agent.
     environment = {
-      systemPackages = [pkgs.yubikey-personalization];
-      shellInit = ''
-        gpg-connect-agent /bye
-        export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-      '';
+      systemPackages = with pkgs; [
+        yubikey-personalization
+        yubioath-flutter
+      ];
+
+      shellInit =
+        # sh
+        ''
+          gpg-connect-agent /bye
+
+          export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+        '';
     };
   };
 }
