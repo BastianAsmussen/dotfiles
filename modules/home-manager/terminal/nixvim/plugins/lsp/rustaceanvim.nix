2e51f4aa27b51ecd711b84a1021a676e897969eb
Bastian Asmussen <bastian@asmussen.tech>
2024-12-05 07:21:08 +0100

    chore: update rustaceanvim configuration for enhanced linting and diagnostics

--- modules/home-manager/terminal/nixvim/plugins/lsp/rustaceanvim.nix
+++ modules/home-manager/terminal/nixvim/plugins/lsp/rustaceanvim.nix
@@ -3,20 +3,58 @@
     enable = true;
 
     settings.server.default_settings.rust-analyzer = {
-      cargo.extraEnv.RUSTFLAGS = builtins.concatStringsSep " " [
-        "-Dclippy::enum_glob_use" # Disallow use of `use` of all enums.
-        "-Dclippy::pedantic" # Enable all pedantic lints.
-        "-Dclippy::nursery" # Enable all nursery lints.
-        "-Dclippy::unwrap_used" # Disallow use of `unwrap`.
-      ];
-
       check.command = "clippy";
-
-      diagnostics.styleLints.enable = true;
-      inlayHints.lifetimeElisionHints.enable = "always";
-      files.excludeDirs = [
-        ".devenv"
+      cargo.extraEnv.RUSTFLAGS = builtins.concatStringsSep " " [
+        "-D clippy::correctness"
+        "-D clippy::pedantic"
+        "-D clippy::nursery"
+        "-D clippy::suspicious"
+        "-D clippy::allow_attributes"
+        "-D clippy::cfg_not_test"
+        "-D clippy::field_scoped_visibility_modifiers"
+        "-W clippy::filetype_is_file"
+        "-W clippy::perf"
+        "-W clippy::complexity"
+        "-W clippy::style"
+        "-W clippy::cargo"
+        "-W clippy::unwrap_used"
+        "-W clippy::assertions_on_result_states"
+        "-W clippy::clone_on_ref_ptr"
+        "-W clippy::empty_drop"
+        "-W clippy::empty_enum_variants_with_brackets"
+        "-W clippy::empty_structs_with_brackets"
+        "-W clippy::default_trait_access"
+        "-W clippy::default_union_representation"
+        "-W clippy::decimal_literal_representation"
+        "-W clippy::arithmetic_side_effects"
+        "-W clippy::shadow_same"
+        "-D deprecated_safe"
+        "-D let_underscore"
+        "-D nonstandard_style"
+        "-W rust_2024_compatibility"
+        "-W future_incompatible"
+        "-W keyword_idents"
       ];
+
+      assist = {
+        emitMustUse = true;
+        expressionFillDefault = "default";
+      };
+
+      completion.termSearch.enable = true;
+      diagnostics.styleLints.enable = true;
+      imports = {
+        granularity.enforce = true;
+        preferPrelude = true;
+      };
+
+      inlayHints = {
+        closureReturnTypeHints.enable = "always";
+        closureStyle = "rust_analyzer";
+        lifetimeElisionHints.enable = "skip_trivial";
+      };
+
+      typing.autoClosingAngleBrackets.enable = true;
     };
   };
 }
