d9240963a9d528b78a99d3d50bd3da9a7bd82f7d
Bastian Asmussen <bastian@asmussen.tech>
2024-12-12 02:36:57 +0100

    chore: add qemu architecture emulation targets

--- modules/nixos/virtualisation/qemu.nix
+++ modules/nixos/virtualisation/qemu.nix
@@ -10,6 +10,12 @@
     virtualisation.libvirtd.enable = true;
     users.extraGroups.libvirt.members = [userInfo.username];
 
+    boot.binfmt.emulatedSystems = [
+      "aarch64-linux"
+      "i686-linux"
+      "riscv64-linux"
+    ];
+
     programs.virt-manager.enable = true;
   };
 }
