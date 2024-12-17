f4087a1373d25ac95f222159f9f69ad659682233
Bastian Asmussen <bastian@asmussen.tech>
2024-12-17 12:33:35 +0100

    feat: enhance fzf configuration with additional options and fd integration

--- modules/home-manager/terminal/fzf.nix
+++ modules/home-manager/terminal/fzf.nix
@@ -1,10 +1,25 @@
-{config, ...}: let
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   cfg = config.programs;
+
+  fd = lib.getBin pkgs.fd;
 in {
   programs.fzf = {
     enable = true;
 
     enableZshIntegration = cfg.zsh.enable;
     tmux.enableShellIntegration = cfg.tmux.enable;
+
+    defaultCommand = "${fd} --type=d --exclude=.git --hidden";
+    fileWidgetCommand = "${fd} --type=f --exclude=.git --hidden";
+    defaultOptions = [
+      "--exact" # Use substring matching by default.
+      "--info=inline"
+      "--no-mouse"
+    ];
   };
 }
