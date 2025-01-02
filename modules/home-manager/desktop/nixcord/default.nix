64f60f518aa93a09ca5ca14c9fa12c141d5825d2
Bastian Asmussen <bastian@asmussen.tech>
2025-01-02 11:27:34 +0100

    chore: switch from vesktop to vencord

--- modules/home-manager/desktop/nixcord/default.nix
+++ modules/home-manager/desktop/nixcord/default.nix
@@ -8,9 +8,6 @@
   programs.nixcord = {
     enable = true;
 
-    discord.enable = false;
-    vesktop.enable = true;
-
     config.themeLinks = [
       "https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css"
     ];
