966c507ee2cf18ac6f2a4ea3c55298bc16f98529
Bastian Asmussen <bastian@asmussen.tech>
2025-01-04 06:07:39 +0100

    chore: refactor LSP configuration to improve modularity and maintainability

--- modules/home-manager/terminal/nixvim/plugins/default.nix
+++ modules/home-manager/terminal/nixvim/plugins/default.nix
@@ -1,10 +1,12 @@
 {
   config,
+  osConfig,
+  lib,
+  userInfo,
   pkgs,
   ...
 }: {
   imports = [
-    ./lsp
     ./snacks
     ./cmp.nix
     ./comment.nix
@@ -23,6 +25,7 @@
       gitsigns = import ./gitsigns.nix config;
       hardtime.enable = true;
       indent-blankline.enable = true;
+      lsp = import ./lsp {inherit config osConfig lib pkgs userInfo;};
       lsp-format.enable = true;
       lspkind = import ./lspkind.nix;
       lualine = import ./lualine.nix;
@@ -30,9 +33,10 @@
       markdown-preview = import ./markdown-preview.nix;
       neocord = import ./neocord.nix config;
       noice.enable = true;
+      none-ls = import ./none-ls.nix;
       oil = import ./oil.nix;
       otter.enable = true;
-      rustaceanvim = import ./rustaceanvim.nix pkgs;
+      rustaceanvim = import ./rustaceanvim.nix {inherit lib config pkgs;};
       sleuth.enable = true;
       treesitter = import ./treesitter.nix pkgs;
       trouble.enable = true;
