fcacbe95658153e867f6343789b2a8bdb0550994
Bastian Asmussen <bastian@asmussen.tech>
2025-01-04 14:44:04 +0100

    chore: create temporary overlay for docker-init package

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,16 +1,23 @@
 {inputs, ...}: {
   # Bring our custom packages from the 'pkgs' directory into scope.
-  additions = final: _prev: import ../pkgs {pkgs = final;};
+  additions = final: _: import ../pkgs {pkgs = final;};
 
   # User-defined overlays.
-  modifications = _final: prev: {
+  modifications = _: prev: {
+    docker-init = prev.docker-init.overrideAttrs (old: {
+      src = prev.fetchurl {
+        url = "https://desktop.docker.com/linux/main/amd64/${old.tag}/docker-desktop-x86_64.pkg.tar.zst";
+        hash = "sha256-pxxlSN2sQqlPUzUPufcK8T+pvdr0cK+9hWTYzwMJv5I=";
+      };
+    });
+
     docker = prev.docker.override {
       initSupport = true;
     };
   };
 
   # Convenient access to the nixpkgs stable branch.
-  stable-packages = final: _prev: {
+  stable-packages = final: _: {
     stable = import inputs.nixpkgs-stable {
       inherit (final) system;
 
