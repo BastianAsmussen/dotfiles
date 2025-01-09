82f214d634e0c3a7d31901719e6e49be377968c7
Bastian Asmussen <bastian@asmussen.tech>
2025-01-09 20:27:05 +0100

    feat: create sin and cos functions

--- lib/default.nix
+++ lib/default.nix
@@ -2,7 +2,7 @@ lib: let
   custom = lib.makeExtensible (_self: let
     callLibs = file: import file {inherit lib;};
   in {
-    math = callLibs ./math.nix;
+    math = callLibs ./math;
     units = callLibs ./units.nix;
   });
 in
---
+++ lib/math/default.nix
@@ -0,0 +1,61 @@
+_: let
+  base = rec {
+    inherit (builtins) floor;
+
+    HALF_PI = 1.5707963267948965579989817342720925807952880859375;
+    PI = 3.141592653589793115997963468544185161590576171875;
+    TAU = 6.28318530717958623199592693708837032318115234375;
+
+    square = a: a * a;
+    cube = a: (square a) * a;
+
+    mod = a: n: let
+      quotient = floor (a / n);
+    in
+      a - (n * quotient);
+
+    max = a: b:
+      if a > b
+      then a
+      else b;
+    min = a: b:
+      if a < b
+      then a
+      else b;
+
+    abs = x:
+      if x < 0.0
+      then (-x)
+      else x;
+
+    pow = base: exp:
+      if exp == 0.0
+      then 1.0
+      else base * pow base (exp - 1.0);
+
+    newtonSqrt = n: x: precision: iteration: maxIterations: let
+      next = (x + (n / x)) / 2.0;
+      diff = abs (next - x);
+    in
+      if iteration >= maxIterations
+      then next
+      else if diff < precision
+      then next
+      else newtonSqrt n next precision (iteration + 1.0) maxIterations;
+    sqrt = n: {
+      precision ? 0.000001,
+      maxIterations ? 100.0,
+    }:
+      if n < 0.0
+      then throw "Cannot calculate square root of negative number!"
+      else if n == 0.0
+      then 0.0
+      else newtonSqrt n (max 1.0 (n / 2.0)) precision 0.0 maxIterations;
+
+    fact = n:
+      if n <= 1.0
+      then 1.0
+      else n * fact (n - 1.0);
+  };
+in
+  base // import ./trigenometry.nix {inherit base;}
