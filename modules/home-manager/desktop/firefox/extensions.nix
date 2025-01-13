496e81954c215a58d8626d8595f39d5fcd798bc8
Bastian Asmussen <bastian@asmussen.tech>
2025-01-13 09:29:12 +0100

    chore: remove skip-redirect Firefox extension
    It too often got in the way by looping infinitely.

--- modules/home-manager/desktop/firefox/extensions.nix
+++ modules/home-manager/desktop/firefox/extensions.nix
@@ -13,7 +13,6 @@
   in {
     "{446900e4-71c2-419f-a6a7-df9c091e268b}".install_url = mkFirefoxURL "bitwarden-password-manager";
     "{74145f27-f039-47ce-a470-a662b129930a}".install_url = mkFirefoxURL "clearurls";
-    "skipredirect@sblask".install_url = mkFirefoxURL "skip-redirect";
     "sponsorBlocker@ajay.app".install_url = mkFirefoxURL "sponsorblock";
     "{762f9885-5a13-4abd-9c77-433dcd38b8fd}".install_url = mkFirefoxURL "return-youtube-dislikes";
     "{34daeb50-c2d2-4f14-886a-7160b24d66a4}".install_url = mkFirefoxURL "youtube-shorts-block";
