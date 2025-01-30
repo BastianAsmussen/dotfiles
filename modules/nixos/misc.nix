63e18cdb845fdf78c020f3811b39023dec49601c
Bastian Asmussen <bastian@asmussen.tech>
2025-01-30 08:14:50 +0100

    chore: re-enable stub-ld
    Yesterday I tried executing a non-patched binary and it told me the file wasn't found,
    taking me over 10 minutes to remember me disabling stub-ld a while back (commit e29f57d).
    In a comment above I called it a fallback for morons, so it only seems reasonable I re-enable it.

--- modules/nixos/misc.nix
+++ modules/nixos/misc.nix
@@ -1,18 +1,10 @@
 {pkgs, ...}: {
-  environment = {
-    # Disable the stub ELF loader added in 24.05 that serves no purpose other than
-    # to throw a warning when you try to run a program that requires dynamic
-    # loading. I disable this since I'm already aware of the implications of NixOS
-    # on dynamic loading, this is exclusively a fallback for morons.
-    stub-ld.enable = false;
+  # Enable system-wide wordlist. Some Pandoc filters and other programs depend
+  # on wordlist available in system path, and shells do not work. I don't like
+  # this, but it's a necessary evil.
+  environment.wordlist = {
+    enable = true;
 
-    # Enable system-wide wordlist. Some Pandoc filters and other programs depend
-    # on wordlist available in system path, and shells do not work. I don't like
-    # this, but it's a necessary evil.
-    wordlist = {
-      enable = true;
-
-      lists.WORDLIST = ["${pkgs.scowl}/share/dict/words.txt"];
-    };
+    lists.WORDLIST = ["${pkgs.scowl}/share/dict/words.txt"];
   };
 }
