306da0c6be9b202a3cbb6d6b0d955f8d1da23031
Bastian Asmussen <bastian@asmussen.tech>
2025-06-23 06:50:33 +0200

    chore: enhance binary cache configuration with domain option

--- modules/nixos/nix/binary-cache.nix
+++ modules/nixos/nix/binary-cache.nix
@@ -5,56 +5,66 @@
   userInfo,
   ...
 }: {
-  options.nix.binaryCache.enable = lib.mkEnableOption "Make this machine host a binary cache.";
-
-  config = lib.mkIf config.nix.binaryCache.enable {
-    nix.settings = {
-      substituters = ["https://cache.asmussen.tech"];
-      trusted-public-keys = [
-        "cache.asmussen.tech:H0C/Z4Hls1uoZb0jj3MsMahWkxZA4Sxn/kw6hyAnnO0="
-      ];
+  options.nix.binaryCache = {
+    enable = lib.mkEnableOption "Make this machine host a binary cache.";
+    domain = lib.mkOption {
+      default = "internal.asmussen.tech";
+      description = "A DNS record pointing to the binary cache.";
+      type = lib.types.str;
     };
+  };
 
-    services = {
-      nix-serve = {
-        enable = true;
-
-        package = pkgs.nix-serve-ng;
-        secretKeyFile = "/var/secrets/cache-private-key.pem";
+  config = let
+    cfg = config.nix.binaryCache;
+  in
+    lib.mkIf config.nix.binaryCache.enable {
+      nix.settings = {
+        substituters = ["https://${cfg.domain}"];
+        trusted-public-keys = [
+          "${cfg.domain}:H0C/Z4Hls1uoZb0jj3MsMahWkxZA4Sxn/kw6hyAnnO0="
+        ];
       };
 
-      nginx = {
-        enable = true;
+      services = {
+        nix-serve = {
+          enable = true;
 
-        recommendedTlsSettings = true;
-        recommendedOptimisation = true;
-        recommendedGzipSettings = true;
-        recommendedProxySettings = true;
+          package = pkgs.nix-serve-ng;
+          secretKeyFile = "/var/secrets/cache-private-key.pem";
+        };
 
-        # Only allow PFS-enabled ciphers with AES-256.
-        sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+        nginx = {
+          enable = true;
 
-        virtualHosts."cache.asmussen.tech" = let
-          cfg = config.services.nix-serve;
-        in {
-          enableACME = true;
-          forceSSL = true;
+          recommendedTlsSettings = true;
+          recommendedOptimisation = true;
+          recommendedGzipSettings = true;
+          recommendedProxySettings = true;
 
-          locations."/".proxyPass = "http://${cfg.bindAddress}:${toString cfg.port}";
+          # Only allow PFS-enabled ciphers with AES-256.
+          sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+
+          virtualHosts."${cfg.domain}" = let
+            cfg' = config.services.nix-serve;
+          in {
+            enableACME = true;
+            forceSSL = true;
+
+            locations."/".proxyPass = "http://${cfg'.bindAddress}:${toString cfg'.port}";
+          };
         };
       };
-    };
 
-    # TLS encryption.
-    users.users.nginx.extraGroups = ["acme"];
-    security.acme = {
-      acceptTerms = true;
-      defaults.email = userInfo.email;
-    };
+      # TLS encryption.
+      users.users.nginx.extraGroups = ["acme"];
+      security.acme = {
+        acceptTerms = true;
+        defaults.email = userInfo.email;
+      };
 
-    networking.firewall.allowedTCPPorts = with config.services.nginx; [
-      defaultHTTPListenPort
-      defaultSSLListenPort
-    ];
-  };
+      networking.firewall.allowedTCPPorts = with config.services.nginx; [
+        defaultHTTPListenPort
+        defaultSSLListenPort
+      ];
+    };
 }
