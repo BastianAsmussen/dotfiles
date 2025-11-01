fe0c11213548ec3496f459ca62650ab1e4328b00
Bastian Asmussen <bastian@asmussen.tech>
2025-11-01 10:30:22 +0100

    chore: remove devenv cachix cache

--- modules/home-manager/terminal/direnv.nix
+++ modules/home-manager/terminal/direnv.nix
@@ -28,9 +28,4 @@
         }
       '';
   };
-
-  nix.settings = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
 }
