39fe34136080f2778d5fd1147381689da6e1ebf5
Bastian Asmussen <bastian@asmussen.tech>
2026-01-26 03:25:07 +0100

    chore: add gtk portal

--- modules/nixos/desktop/environment/hyprland.nix
+++ modules/nixos/desktop/environment/hyprland.nix
@@ -72,5 +72,17 @@ in {
 
       xwayland.enable = true;
     };
+
+    xdg.portal = {
+      enable = true;
+
+      xdgOpenUsePortal = true;
+      config = {
+        common.default = ["gtk"];
+        hyprland.default = ["gtk" "hyprland"];
+      };
+
+      extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    };
   };
 }
