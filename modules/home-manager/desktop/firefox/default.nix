d6510319221683c8f84e3d255dc66b8ee531830e
Bastian Asmussen <bastian@asmussen.tech>
2026-02-08 08:36:55 +0100

    chore: remove wayland firefox environment variable
    It has been causing images to render improperly in the browser for quite a while now.

--- modules/home-manager/desktop/firefox/default.nix
+++ modules/home-manager/desktop/firefox/default.nix
@@ -1,17 +1,11 @@
 {
   inputs,
-  lib,
   osConfig,
   ...
 }: {
   imports = [
     inputs.schizofox.homeManagerModule
   ];
-
-  # If we're on Wayland, tell that to Firefox.
-  home.sessionVariables = lib.mkIf osConfig.desktop.greeter.useWayland {
-    MOZ_ENABLE_WAYLAND = "1";
-  };
 
   stylix.targets.firefox.enable = false;
   programs.schizofox = {
