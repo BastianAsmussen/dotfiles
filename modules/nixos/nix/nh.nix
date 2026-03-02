7317691e31be79e32934a92c4b5c6f28ad119569
Bastian Asmussen <bastian@asmussen.tech>
2026-03-02 08:21:30 +0100

    chore: update nh.nix to include additional nix-related tools

--- modules/nixos/nix/nh.nix
+++ modules/nixos/nix/nh.nix
@@ -1,13 +1,16 @@
-{userInfo, ...}: {
+{
+  userInfo,
+  pkgs,
+  ...
+}: {
   programs.nh = {
     enable = true;
 
-    clean = {
-      enable = true;
-
-      extraArgs = "--keep-since 7d --keep 3";
-    };
-
     flake = "/home/${userInfo.username}/dotfiles";
   };
+
+  environment.systemPackages = with pkgs; [
+    nix-output-monitor
+    nvd
+  ];
 }
