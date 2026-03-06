2de57d59ac1dfc656fc576ca2f0c7ee9e196bd60
Bastian Asmussen <bastian@asmussen.tech>
2026-03-06 20:17:02 +0100

    chore: integrate nvtop for NVIDIA monitoring

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -46,21 +46,24 @@ in {
     # Load NVIDIA drivers for Xorg and Wayland.
     services.xserver.videoDrivers = ["nvidia"];
 
-    environment.variables = {
-      # Required to run the correct GBM backend for NVIDIA GPUs on Wayland.
-      GBM_BACKEND = mkIf config.desktop.greeter.useWayland "nvidia-drm";
-      # Apparently, without this nouveau may attempt to be used instead.
-      # (despite it being blacklisted)
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    environment = {
+      systemPackages = [pkgs.nvtopPackages.nvidia];
+      variables = {
+        # Required to run the correct GBM backend for NVIDIA GPUs on Wayland.
+        GBM_BACKEND = mkIf config.desktop.greeter.useWayland "nvidia-drm";
+        # Apparently, without this nouveau may attempt to be used instead.
+        # (despite it being blacklisted)
+        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
 
-      LIBVA_DRIVER_NAME = "nvidia";
-      __GL_GSYNC_ALLOWED = "1";
-      __GL_VRR_ALLOWED = "0";
-      WLR_DRM_NO_ATOMIC = "1";
-      WLR_RENDERER = "vulkan";
+        LIBVA_DRIVER_NAME = "nvidia";
+        __GL_GSYNC_ALLOWED = "1";
+        __GL_VRR_ALLOWED = "0";
+        WLR_DRM_NO_ATOMIC = "1";
+        WLR_RENDERER = "vulkan";
 
-      # Hardware cursors are currently broken on NVIDIA.
-      WLR_NO_HARDWARE_CURSORS = "1";
+        # Hardware cursors are currently broken on NVIDIA.
+        WLR_NO_HARDWARE_CURSORS = "1";
+      };
     };
   };
 }
