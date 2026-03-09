81197c92d53b8fb35f6e7527f8ded65ea158ea4c
Bastian Asmussen <bastian@asmussen.tech>
2026-03-09 06:56:15 +0100

    chore: comment out QEMU configuration in virtualisation module
    Temporarily disable QEMU settings due to an issue (fixed in https://github.com/NixOS/nixpkgs/pull/496839).

--- modules/nixosModules/features/virtualisation.nix
+++ modules/nixosModules/features/virtualisation.nix
@@ -91,19 +91,19 @@
           };
         }
 
-        # QEMU
-        {
-          virtualisation.libvirtd.enable = true;
-          users.extraGroups.libvirt.members = [userInfo.username];
-
-          boot.binfmt.emulatedSystems = [
-            "aarch64-linux"
-            "i686-linux"
-            "riscv64-linux"
-          ];
-
-          programs.virt-manager.enable = true;
-        }
+        # # QEMU
+        # {
+        #   virtualisation.libvirtd.enable = true;
+        #   users.extraGroups.libvirt.members = [userInfo.username];
+        #
+        #   boot.binfmt.emulatedSystems = [
+        #     "aarch64-linux"
+        #     "i686-linux"
+        #     "riscv64-linux"
+        #   ];
+        #
+        #   programs.virt-manager.enable = true;
+        # }
       ];
   };
 }
