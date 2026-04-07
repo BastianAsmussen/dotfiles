931d4fb489aacc1496613bb1034ec44da3630b3f
Bastian Asmussen <bastian@asmussen.tech>
2026-04-07 05:49:33 +0200

    fix: use client:supports_method instead of deprecated client.supports_method

--- modules/homeManagerModules/_nixvim-config.nix
+++ modules/homeManagerModules/_nixvim-config.nix
@@ -621,7 +621,7 @@
           -- of the word under the cursor when your cursor rests there for a
           -- little while. When you move your cursor, the highlights will be
           -- cleared (the second autocommand).
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = bufnr,
@@ -647,7 +647,7 @@
           -- The following autocommand is used to enable inlay hints in your
           -- code, if the language server you are using supports them.
           -- This may be unwanted, since they displace some of your code
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
             map('<leader>th', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
