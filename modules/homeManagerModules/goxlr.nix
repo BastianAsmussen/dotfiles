4b35c750e68974b33e85dd05f957dea024483d21
Bastian Asmussen <bastian@asmussen.tech>
2026-05-14 03:45:24 +0200

    fix(goxlr): add hm-managed settings.json

--- modules/homeManagerModules/goxlr.nix
+++ modules/homeManagerModules/goxlr.nix
@@ -1,12 +1,49 @@
 {
   flake.homeModules.goxlr =
     let
-      configDir = ".local/share/goxlr-utility";
+      dataDir = ".local/share/goxlr-utility";
+      configDir = ".config/goxlr-utility";
     in
     {
       home.file = {
-        "${configDir}/profiles".source = ./goxlr-config/profiles;
-        "${configDir}/mic-profiles".source = ./goxlr-config/mic-profiles;
+        "${dataDir}/profiles".source = ./goxlr-config/profiles;
+        "${dataDir}/mic-profiles".source = ./goxlr-config/mic-profiles;
+        "${configDir}/settings.json".text = builtins.toJSON {
+          show_tray_icon = true;
+          selected_locale = null;
+          tts_enabled = false;
+          allow_network_access = false;
+          macos_handle_aggregates = true;
+          profile_directory = null;
+          mic_profile_directory = null;
+          samples_directory = null;
+          presets_directory = null;
+          icons_directory = null;
+          logs_directory = null;
+          backup_directory = null;
+          log_level = "Debug";
+          open_ui_on_launch = false;
+          activate = null;
+          firmware_source = "Live";
+          devices."S220105246CQK" = {
+            profile = "Default";
+            mic_profile = "DEFAULT";
+            hold_delay = 500;
+            sampler_pre_buffer = null;
+            chat_mute_mutes_mic_to_chat = true;
+            lock_faders = false;
+            enable_monitor_with_fx = false;
+            sampler_reset_on_clear = true;
+            sampler_fade_duration = 500;
+            vod_mode = "Routable";
+            shutdown_commands = [ { LoadProfileColours = "Sleep"; } ];
+            sleep_commands = [ ];
+            wake_commands = [ ];
+          };
+
+          sample_gain = { };
+        };
+
       };
     };
 }
