0a74489d6fd22c590894d412482c278d514fcd32
Bastian Asmussen <bastian@asmussen.tech>
2026-06-11 08:36:52 +0200

    chore(distrobox): add more Nix-specific mounts

--- modules/homeManagerModules/distrobox.nix
+++ modules/homeManagerModules/distrobox.nix
@@ -5,7 +5,7 @@
       home.packages = [ pkgs.distrobox ];
 
       xdg.configFile."distrobox/distrobox.conf".text = ''
-        container_additional_volumes="/nix/store:/nix/store:ro /etc/static/profiles/per-user:/etc/profiles/per-user:ro"
+        container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
         container_always_pull="0"
         container_image_default="archlinux/archlinux:latest"
         container_name_default="archlinux"
