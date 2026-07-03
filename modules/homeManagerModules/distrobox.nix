03dbbc5cbb7f1194d3ca4d97dbfa266302444ba8
Bastian Asmussen <bastian@asmussen.tech>
2026-07-03 11:12:12 +0200

    chore(distrobox): set up Nix profile in containers

--- modules/homeManagerModules/distrobox.nix
+++ modules/homeManagerModules/distrobox.nix
@@ -10,6 +10,9 @@
         container_image_default="docker.io/library/archlinux:latest"
         container_name_default="archlinux"
         non_interactive="1"
+        init_hooks="echo 'export PATH=$HOME/.nix-profile/bin:$PATH
+        export NIX_SSL_CERT_FILE=$HOME/.nix-profile/etc/ssl/certs/ca-bundle.crt' > /etc/profile.d/nix-user-profile.sh"
+        pre_init_hooks="source /etc/profile.d/nix-user-profile.sh 2>/dev/null || true"
       '';
     };
 }
