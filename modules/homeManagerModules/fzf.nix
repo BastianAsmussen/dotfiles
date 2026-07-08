fb5439390ad22027093ac712788b1d5ad1cde106
Bastian Asmussen <bastian@asmussen.tech>
2026-07-08 14:32:50 +0200

    chore(fzf): rename fileWidgetCommand to fileWidget.command

--- modules/homeManagerModules/fzf.nix
+++ modules/homeManagerModules/fzf.nix
@@ -16,7 +16,7 @@
         enableZshIntegration = cfg.zsh.enable;
         tmux.enableShellIntegration = cfg.tmux.enable;
         defaultCommand = "${fd} --type=d --exclude=.git --hidden";
-        fileWidgetCommand = "${fd} --type=f --exclude=.git --hidden";
+        fileWidget.command = "${fd} --type=f --exclude=.git --hidden";
         defaultOptions = [
           "--exact" # Use substring matching by default.
           "--info=inline"
