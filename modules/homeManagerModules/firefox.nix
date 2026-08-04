e11d5f686fc3178303c7ed7e405fc0aa5af700eb
Bastian Asmussen <bastian@asmussen.tech>
2026-08-04 08:39:57 +0200

    fix(schizofox): set as XDG default browser again

--- modules/homeManagerModules/firefox.nix
+++ modules/homeManagerModules/firefox.nix
@@ -140,5 +140,21 @@
           }
         );
       };
+
+      xdg.mimeApps = {
+        enable = true;
+        defaultApplications =
+          let
+            browser = "Schizofox.desktop";
+          in
+          {
+            "text/html" = browser;
+            "application/pdf" = browser;
+            "x-scheme-handler/http" = browser;
+            "x-scheme-handler/https" = browser;
+            "x-scheme-handler/about" = browser;
+            "x-scheme-handler/unknown" = browser;
+          };
+      };
     };
 }
